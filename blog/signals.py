ab2ab2bcfde015aa9b08d4edf423e39bec5180e9
yhwh371224 <sungkam3@gmail.com>
2023-03-03 21:45:23 +1100

    signals.py > no email, just pass

--- blog/signals.py
+++ blog/signals.py
@@ -14,24 +14,25 @@ from django.core.mail import send_mail
 @receiver(post_save, sender=Post)
 def notify_user(sender, instance, created, **kwargs):
     if instance.is_confirmed:
-        html_content = render_to_string("basecamp/html_email-confirmation.html",
-                                        {'name': instance.name, 'contact': instance.contact, 'email': instance.email,
-                                         'flight_date': instance.flight_date, 'flight_number': instance.flight_number,
-                                         'flight_time': instance.flight_time, 'pickup_time': instance.pickup_time,
-                                         'direction': instance.direction, 'street': instance.street, 'suburb': instance.suburb,
-                                         'no_of_passenger': instance.no_of_passenger, 'no_of_baggage': instance.no_of_baggage,
-                                         'message': instance.message, 'price': instance.price, 'paid': instance.paid })
+        pass
+        # html_content = render_to_string("basecamp/html_email-confirmation.html",
+        #                                 {'name': instance.name, 'contact': instance.contact, 'email': instance.email,
+        #                                  'flight_date': instance.flight_date, 'flight_number': instance.flight_number,
+        #                                  'flight_time': instance.flight_time, 'pickup_time': instance.pickup_time,
+        #                                  'direction': instance.direction, 'street': instance.street, 'suburb': instance.suburb,
+        #                                  'no_of_passenger': instance.no_of_passenger, 'no_of_baggage': instance.no_of_baggage,
+        #                                  'message': instance.message, 'price': instance.price, 'paid': instance.paid })
 
-        text_content = strip_tags(html_content)
+        # text_content = strip_tags(html_content)
 
-        email = EmailMultiAlternatives(
-            "Booking confirmation - EasyGo",
-            text_content,
-            '',
-            ['info@easygoshuttle.com.au']
-        )
-        email.attach_alternative(html_content, "text/html")
-        email.send()
+        # email = EmailMultiAlternatives(
+        #     "Booking confirmation - EasyGo",
+        #     text_content,
+        #     '',
+        #     ['info@easygoshuttle.com.au']
+        # )
+        # email.attach_alternative(html_content, "text/html")
+        # email.send()
 
     else:
         pass
@@ -41,39 +42,41 @@ def notify_user(sender, instance, created, **kwargs):
 @receiver(post_save, sender=Inquiry)
 def notify_user(sender, instance, created, **kwargs):
     if instance.cancelled:
-        html_content = render_to_string("basecamp/html_email-cancelled.html",
-                                        {'name': instance.name, 'email': instance.email,
-                                         })
-        text_content = strip_tags(html_content)
+        pass
+        # html_content = render_to_string("basecamp/html_email-cancelled.html",
+        #                                 {'name': instance.name, 'email': instance.email,
+        #                                  })
+        # text_content = strip_tags(html_content)
 
-        email = EmailMultiAlternatives(
-            "Booking Inquiry - EasyGo",
-            text_content,
-            '',
-            ['info@easygoshuttle.com.au']
-        )
-        email.attach_alternative(html_content, "text/html")
-        email.send()
+        # email = EmailMultiAlternatives(
+        #     "Booking Inquiry - EasyGo",
+        #     text_content,
+        #     '',
+        #     ['info@easygoshuttle.com.au']
+        # )
+        # email.attach_alternative(html_content, "text/html")
+        # email.send()
 
     elif instance.is_confirmed:
-        html_content = render_to_string("basecamp/html_email-inquiry-response.html",
-                                        {'name': instance.name, 'contact': instance.contact, 'email': instance.email,
-                                         'flight_date': instance.flight_date, 'flight_number': instance.flight_number,
-                                         'flight_time': instance.flight_time, 'pickup_time': instance.pickup_time,
-                                         'direction': instance.direction, 'street': instance.street, 'suburb': instance.suburb,
-                                         'no_of_passenger': instance.no_of_passenger, 'no_of_baggage': instance.no_of_baggage,
-                                         'message': instance.message, 'price': instance.price})
+        pass
+        # html_content = render_to_string("basecamp/html_email-inquiry-response.html",
+        #                                 {'name': instance.name, 'contact': instance.contact, 'email': instance.email,
+        #                                  'flight_date': instance.flight_date, 'flight_number': instance.flight_number,
+        #                                  'flight_time': instance.flight_time, 'pickup_time': instance.pickup_time,
+        #                                  'direction': instance.direction, 'street': instance.street, 'suburb': instance.suburb,
+        #                                  'no_of_passenger': instance.no_of_passenger, 'no_of_baggage': instance.no_of_baggage,
+        #                                  'message': instance.message, 'price': instance.price})
 
-        text_content = strip_tags(html_content)
+        # text_content = strip_tags(html_content)
 
-        email = EmailMultiAlternatives(
-            "Booking Inquiry - EasyGo",
-            text_content,
-            '',
-            ['info@easygoshuttle.com.au']
-        )
-        email.attach_alternative(html_content, "text/html")
-        email.send()
+        # email = EmailMultiAlternatives(
+        #     "Booking Inquiry - EasyGo",
+        #     text_content,
+        #     '',
+        #     ['info@easygoshuttle.com.au']
+        # )
+        # email.attach_alternative(html_content, "text/html")
+        # email.send()
 
     else:
         pass
