01e2d12d45edb19b6c876ec69b8b7a5e29c882f8
Ubuntu <ubuntu@ip-172-31-19-243.ap-southeast-2.compute.internal>
2023-03-05 10:25:47 +0000

    views and htmls

--- basecamp/views.py
+++ basecamp/views.py
@@ -1502,7 +1502,7 @@ def re_confirm_email_detail(request):
         else:
             name = user.name
         
-        html_content = render_to_string("basecamp/html_email-reconfirmemailonly.html",
+        html_content = render_to_string("basecamp/html_email-confirmation.html",
                                     {'name': user.name, 'contact': user.contact, 'email': user.email,
                                      'flight_date': user.flight_date, 'flight_number': user.flight_number,
                                      'flight_time': user.flight_time, 'pickup_time': user.pickup_time,
