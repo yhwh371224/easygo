d725044edd477d69ef1155c6940f57f5d962472c
Ubuntu <ubuntu@ip-172-31-19-243.ap-southeast-2.compute.internal>
2023-03-15 18:15:17 +1100

    html email confirmation and inquiry

--- blog/signals.py
+++ blog/signals.py
@@ -12,26 +12,35 @@ from time import sleep
 
 
 @receiver(post_save, sender=Post)
-def notify_user(sender, instance, created, **kwargs):
-    
-    if instance.return_flight_number:   
-        
-        p = Post(name=instance.name, contact=instance.contact, email=instance.email, flight_date=instance.return_flight_date, 
-                 flight_number=instance.return_flight_number, flight_time=instance.return_flight_time, pickup_time=instance.return_pickup_time, 
-                 direction=instance.direction, suburb=instance.suburb, street=instance.street, no_of_passenger=instance.no_of_passenger, 
-                 no_of_baggage=instance.no_of_baggage, message=instance.message, price=instance.price, paid=instance.paid)
-        
-        p.save()       
-
-        sleep(1)
+def notify_user(email):
+    user = Post.objects.filter(email=email).first()
+    if user and user.return_flight_number:
+        p = Post(
+            name=user.name, 
+            contact=user.contact, 
+            email=user.email, 
+            flight_date=user.return_flight_date,
+            flight_number=user.return_flight_number, 
+            flight_time=user.return_flight_time, 
+            pickup_time=user.pickup_time, 
+            direction=user.direction, 
+            suburb=user.suburb, 
+            street=user.street, 
+            no_of_passenger=user.no_of_passenger, 
+            no_of_baggage=user.no_of_baggage, 
+            message=user.message, 
+            price=user.price, 
+            paid=user.paid
+        )
+        p.save()
                         
         html_content = render_to_string("basecamp/html_email-confirmation-return.html",
-                                        {'name': instance.name, 'contact': instance.contact, 'email': instance.email,
-                                         'flight_date': instance.flight_date, 'flight_number': instance.flight_number,
-                                         'flight_time': instance.flight_time, 'pickup_time': instance.pickup_time,
-                                         'direction': instance.direction, 'street': instance.street, 'suburb': instance.suburb,
-                                         'no_of_passenger': instance.no_of_passenger, 'no_of_baggage': instance.no_of_baggage,
-                                         'message': instance.message, 'notice': instance.notice, 'price': instance.price, 'paid': instance.paid })
+                                        {'name': user.name, 'contact': user.contact, 'email': user.email,
+                                         'flight_date': user.return_flight_date, 'flight_number': user.return_flight_number,
+                                         'flight_time': user.return_flight_time, 'pickup_time': user.pickup_time,
+                                         'direction': user.direction, 'street': user.street, 'suburb': user.suburb,
+                                         'no_of_passenger': user.no_of_passenger, 'no_of_baggage': user.no_of_baggage,
+                                         'message': user.message, 'notice': user.notice, 'price': user.price, 'paid': user.paid })
 
         text_content = strip_tags(html_content)
 
@@ -39,7 +48,7 @@ def notify_user(sender, instance, created, **kwargs):
             "Booking confirmation - EasyGo",
             text_content,
             '',
-            [instance.email, 'info@easygoshuttle.com.au']
+            [user.email, 'info@easygoshuttle.com.au']
         )
         email.attach_alternative(html_content, "text/html")
         
