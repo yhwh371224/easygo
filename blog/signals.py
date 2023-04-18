15c8eec3213651a10a1afae614753a9bfd6e7928
Ubuntu <ubuntu@ip-172-31-19-243.ap-southeast-2.compute.internal>
2023-04-18 19:00:04 +1000

    signals.py

--- blog/signals.py
+++ blog/signals.py
@@ -6,48 +6,34 @@ from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 
 
-
 @receiver(post_save, sender=Post)
 def notify_user_post(sender, instance, created, **kwargs):
-    
     if instance.return_flight_number:
-        
         p = Post(name=instance.name, contact=instance.contact, email=instance.email, flight_date=instance.return_flight_date, 
                  flight_number=instance.return_flight_number, flight_time=instance.return_flight_time, pickup_time=instance.return_pickup_time, 
                  direction=instance.return_direction, suburb=instance.suburb, street=instance.street, no_of_passenger=instance.no_of_passenger, 
                  no_of_baggage=instance.no_of_baggage, message=instance.message, 
                  notice=instance.notice, price=instance.price, paid=instance.paid)
-        
         p.save() 
-        
-    return 
-    
     
 
 @receiver(post_save, sender=Inquiry)
 def notify_user_inquiry(sender, instance, created, **kwargs):
-    
-    if instance.cancelled:        
-       
+    if instance.cancelled:
         html_content = render_to_string("basecamp/html_email-cancelled.html",
                                         {'name': instance.name, 'email': instance.email,
                                          })
-        
         text_content = strip_tags(html_content)
-
         email = EmailMultiAlternatives(
             "Booking Inquiry - EasyGo",
             text_content,
             '',
             [instance.email, 'info@easygoshuttle.com.au']
         )
-        
         email.attach_alternative(html_content, "text/html")
-        
         email.send()
 
-    elif instance.is_confirmed:       
-        
+    elif instance.is_confirmed:    
         html_content = render_to_string("basecamp/html_email-inquiry-response.html",
                                         {'name': instance.name, 'contact': instance.contact, 'email': instance.email,
                                          'flight_date': instance.flight_date, 'flight_number': instance.flight_number,
@@ -58,17 +44,13 @@ def notify_user_inquiry(sender, instance, created, **kwargs):
                                          'return_flight_number': instance.return_flight_number, 'return_flight_time': instance.return_flight_time,
                                          'return_pickup_time': instance.return_pickup_time, 'message': instance.message, 'price': instance.price, 
                                          'notice': instance.notice,})
-
         text_content = strip_tags(html_content)
-
         email = EmailMultiAlternatives(
             "Booking Inquiry - EasyGo",
             text_content,
             '',
             [instance.email, 'info@easygoshuttle.com.au']
         )
-        
         email.attach_alternative(html_content, "text/html")
-        
         email.send()
     
