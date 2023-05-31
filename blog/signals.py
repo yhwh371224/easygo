da55ab99a222e1f3d9c3199d101aa380a339d884
Ubuntu <ubuntu@ip-172-31-19-243.ap-southeast-2.compute.internal>
2023-05-31 23:29:37 +1000

    booking.html > company_name & email1 added

--- blog/signals.py
+++ blog/signals.py
@@ -24,11 +24,11 @@ from django.db.models import Q
 
 @receiver(post_save, sender=Post)
 def notify_user_post(sender, instance, created, **kwargs):
-    if instance.return_flight_number and not instance.paid:
-        p = Post(name=instance.name, contact=instance.contact, email=instance.email, flight_date=instance.return_flight_date, 
-                 flight_number=instance.return_flight_number, flight_time=instance.return_flight_time, pickup_time=instance.return_pickup_time, 
-                 direction=instance.return_direction, suburb=instance.suburb, street=instance.street, no_of_passenger=instance.no_of_passenger, 
-                 no_of_baggage=instance.no_of_baggage, message=instance.message, return_pickup_time="x",
+    if not instance.paid and instance.return_flight_number:
+        p = Post(name=instance.name, contact=instance.contact, email=instance.email, company_name=instance.company_name, email1=instance.email1, 
+                 flight_date=instance.return_flight_date, flight_number=instance.return_flight_number, flight_time=instance.return_flight_time, 
+                 pickup_time=instance.return_pickup_time, direction=instance.return_direction, suburb=instance.suburb, street=instance.street, 
+                 no_of_passenger=instance.no_of_passenger, no_of_baggage=instance.no_of_baggage, message=instance.message, return_pickup_time="x",
                  notice=instance.notice, price=instance.price, paid=instance.paid)
         p.save() 
     
