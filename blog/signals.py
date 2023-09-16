a796f34b6c1b4af91bf384b6111917b70a38172e
Ubuntu <ubuntu@ip-172-31-19-243.ap-southeast-2.compute.internal>
2023-09-17 08:10:01 +1000

    views.py > is_confirmed applied to confirmation and booking

--- blog/signals.py
+++ blog/signals.py
@@ -208,47 +208,47 @@ SCOPES = ['https://www.googleapis.com/auth/calendar']
 
 @receiver(post_save, sender=Post)
 def create_event_on_calendar(sender, instance, created, **kwargs):
+    if instance.is_confirmed:
     
-    google_credentials = service_account.Credentials.from_service_account_file(
-        SERVICE_ACCOUNT_KEY_FILE, scopes=SCOPES
-    )
-   
-    service = build('calendar', 'v3', credentials=google_credentials)
-   
-    if created:
-        # Create a Google Calendar API service object
-        service = build('calendar', 'v3', credentials=google_credentials)        
-        # Call the funtion that creates the event in Google Calendar
-        title = " ".join([instance.pickup_time, instance.flight_number, instance.flight_time, 'p'+str(instance.no_of_passenger), '$'+instance.price, instance.contact])
-        address = " ".join([instance.street, instance.suburb])
-        message = " ".join([instance.name, instance.email, instance.no_of_baggage, instance.message, str(instance.return_flight_date)])
-        # flight_date = datetime.datetime.combine(instance.flight_date, datetime.time())
-        flight_date = datetime.datetime.strptime(str(instance.flight_date), '%Y-%m-%d')
-        pickup_time = datetime.datetime.strptime(instance.pickup_time, '%H:%M')
-        start = datetime.datetime.combine(flight_date.date(), pickup_time.time())        
-        end = start + datetime.timedelta(hours=1)
+        google_credentials = service_account.Credentials.from_service_account_file(
+            SERVICE_ACCOUNT_KEY_FILE, scopes=SCOPES
+        )
+    
+        service = build('calendar', 'v3', credentials=google_credentials)
+    
+        if created:
+            # Create a Google Calendar API service object
+            service = build('calendar', 'v3', credentials=google_credentials)        
+            # Call the funtion that creates the event in Google Calendar
+            title = " ".join([instance.pickup_time, instance.flight_number, instance.flight_time, 'p'+str(instance.no_of_passenger), '$'+instance.price, instance.contact])
+            address = " ".join([instance.street, instance.suburb])
+            message = " ".join([instance.name, instance.email, instance.no_of_baggage, instance.message, str(instance.return_flight_date)])
+            # flight_date = datetime.datetime.combine(instance.flight_date, datetime.time())
+            flight_date = datetime.datetime.strptime(str(instance.flight_date), '%Y-%m-%d')
+            pickup_time = datetime.datetime.strptime(instance.pickup_time, '%H:%M')
+            start = datetime.datetime.combine(flight_date.date(), pickup_time.time())        
+            end = start + datetime.timedelta(hours=1)    
 
+            event = {
+                'summary': title,
+                'location': address,
+                'start': {
+                    'dateTime': start.strftime('%Y-%m-%dT%H:%M:%S'),
+                    'timeZone': '(GMT+10:00) Eastern Australia Time - Sydney',
+                },
+                'end': {
+                    'dateTime': end.strftime('%Y-%m-%dT%H:%M:%S'),
+                    'timeZone': '(GMT+10:00) Eastern Australia Time - Sydney',
+                },
+                'description': message,
+            }   
 
-        event = {
-            'summary': title,
-            'location': address,
-            'start': {
-                'dateTime': start.strftime('%Y-%m-%dT%H:%M:%S'),
-                'timeZone': '(GMT+10:00) Eastern Australia Time - Sydney',
-            },
-            'end': {
-                'dateTime': end.strftime('%Y-%m-%dT%H:%M:%S'),
-                'timeZone': '(GMT+10:00) Eastern Australia Time - Sydney',
-            },
-            'description': message,
-        }   
-        
-    try:
-        event = service.events().insert(calendarId='info@easygoshuttle.com.au', body=event).execute()        
-        print('Event created: %s' % (event.get('htmlLink')))
-       
-    except HttpError as error:
-        print(f'An error occurred: {error}')
+        try:
+            event = service.events().insert(calendarId='primary', body=event).execute()        
+            print('Event created: %s' % (event.get('htmlLink')))
+
+        except HttpError as error:
+            print(f'An error occurred: {error}')
 
 
 
