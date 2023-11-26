ab23f039089756157262133e0b6d8eb0c23cec84
sung kam <sungkam3@gmail.com>
2023-11-26 21:19:35 +0000

    third party packages updated

--- main/urls.py
+++ main/urls.py
@@ -1,18 +1,3 @@
-"""main URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -20,10 +5,10 @@ from django.conf.urls.static import static
 from django.views.static import serve
 
 
-urlpatterns = [
-    path('horeb_yhwh/defender/', include('defender.urls')),    
+urlpatterns = [  
     path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),    
     path('horeb_yhwh/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('', include('basecamp.urls')),
     path('accounts/', include('allauth.urls')),
