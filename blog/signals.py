e9165cf6764ca3a05f51358c79f43d4f4fdac82f
sung kam <sungkam3@gmail.com>
2023-12-07 22:36:42 +1100

    tasks.py and signals.py for celery worker for calendar jobs

--- blog/signals.py
+++ blog/signals.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 from main.settings import RECIPIENT_EMAIL
 from .models import Post, Inquiry, Payment
+from .tasks import create_event_on_calendar
 from basecamp.models import Inquiry_point
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -25,35 +26,19 @@ from google.auth.transport.requests import Request
 # Flight return booking
 @receiver(post_save, sender=Post)
 def notify_user_post(sender, instance, created, **kwargs):
-    if not instance.calendar_event_id and instance.return_flight_number:
+    if instance.return_pickup_time != "x" and instance.return_flight_number:
         p = Post(name=instance.name, contact=instance.contact, email=instance.email, company_name=instance.company_name, email1=instance.email1, 
                  flight_date=instance.return_flight_date, flight_number=instance.return_flight_number, flight_time=instance.return_flight_time, 
                  pickup_time=instance.return_pickup_time, direction=instance.return_direction, suburb=instance.suburb, street=instance.street, 
                  no_of_passenger=instance.no_of_passenger, no_of_baggage=instance.no_of_baggage, message=instance.message, return_pickup_time="x",
                  return_flight_date=instance.flight_date, notice=instance.notice, price=instance.price, paid=instance.paid, driver=instance.driver)
         p.save() 
-
-        sleep(3)
     
     
 # Flight inquiry
 @receiver(post_save, sender=Inquiry)
 def notify_user_inquiry(sender, instance, created, **kwargs):
-    if instance.cancelled:
-        html_content = render_to_string("basecamp/html_email-cancelled.html",
-                                        {'name': instance.name, 'email': instance.email,
-                                         })
-        text_content = strip_tags(html_content)
-        email = EmailMultiAlternatives(
-            "Booking Inquiry - EasyGo",
-            text_content,
-            '',
-            [instance.email]
-        )
-        email.attach_alternative(html_content, "text/html")
-        email.send()
-
-    elif instance.is_confirmed:    
+    if instance.is_confirmed:    
         html_content = render_to_string("basecamp/html_email-inquiry-response.html",
                                         {'company_name': instance.company_name, 'name': instance.name, 'contact': instance.contact, 'email': instance.email,
                                          'flight_date': instance.flight_date, 'flight_number': instance.flight_number,
@@ -74,17 +59,13 @@ def notify_user_inquiry(sender, instance, created, **kwargs):
         email.attach_alternative(html_content, "text/html")
         email.send()
 
-
-# Point to point inquiry
-@receiver(post_save, sender=Inquiry_point)
-def notify_user_inquiry_point(sender, instance, created, **kwargs):
-    if instance.cancelled:
+    elif instance.cancelled:
         html_content = render_to_string("basecamp/html_email-cancelled.html",
                                         {'name': instance.name, 'email': instance.email,
                                          })
         text_content = strip_tags(html_content)
         email = EmailMultiAlternatives(
-            "EasyGo Booking Inquiry",
+            "Booking Inquiry - EasyGo",
             text_content,
             '',
             [instance.email]
@@ -92,7 +73,11 @@ def notify_user_inquiry_point(sender, instance, created, **kwargs):
         email.attach_alternative(html_content, "text/html")
         email.send()
 
-    elif instance.is_confirmed:    
+
+# Point to point inquiry
+@receiver(post_save, sender=Inquiry_point)
+def notify_user_inquiry_point(sender, instance, created, **kwargs):
+    if instance.is_confirmed:    
         html_content = render_to_string("basecamp/html_email-inquiry-response-p2p.html",
                                         {'name': instance.name, 'contact': instance.contact, 'email': instance.email,
                                          'flight_date': instance.flight_date, 'pickup_time': instance.pickup_time, 'flight_number': instance.flight_number,
@@ -110,8 +95,22 @@ def notify_user_inquiry_point(sender, instance, created, **kwargs):
         email.attach_alternative(html_content, "text/html")
         email.send()      
 
+    elif instance.cancelled:
+        html_content = render_to_string("basecamp/html_email-cancelled.html",
+                                        {'name': instance.name, 'email': instance.email,
+                                         })
+        text_content = strip_tags(html_content)
+        email = EmailMultiAlternatives(
+            "EasyGo Booking Inquiry",
+            text_content,
+            '',
+            [instance.email]
+        )
+        email.attach_alternative(html_content, "text/html")
+        email.send()
 
-# PayPal Payment & Google Calendar payment update
+    
+# PayPal Payment 
 @receiver(post_save, sender=Payment)
 def notify_user_payment(sender, instance, created, **kwargs):      
     post_name = Post.objects.filter(
@@ -135,8 +134,6 @@ def notify_user_payment(sender, instance, created, **kwargs):
         post_name.paid = instance.gross_amount
         post_name.save()
 
-        sleep(1)
-
         if post_name.return_pickup_time == 'x':
                 post_name_second = Post.objects.filter(email=post_name.email)[1]
                 post_name_second.paid = instance.gross_amount
@@ -159,71 +156,77 @@ def notify_user_payment(sender, instance, created, **kwargs):
 
 ## google calendar recording 
 @receiver(post_save, sender=Post)
-def create_event_on_calendar(sender, instance, created, **kwargs):
+def async_create_event_on_calendar(sender, instance, created, **kwargs):
+    create_event_on_calendar.delay(instance.id)
+
+
+
+# @receiver(post_save, sender=Post)
+# def create_event_on_calendar(sender, instance, created, **kwargs):
     
-        SCOPES = ['https://www.googleapis.com/auth/calendar']
+#         SCOPES = ['https://www.googleapis.com/auth/calendar']
     
-        creds = None
-        secure_directory = 'secure/'
-        token_file_path = os.path.join(secure_directory, 'token.json')
+#         creds = None
+#         secure_directory = 'secure/'
+#         token_file_path = os.path.join(secure_directory, 'token.json')
     
-        if os.path.exists(token_file_path):
-            creds = Credentials.from_authorized_user_file(token_file_path, SCOPES)   
+#         if os.path.exists(token_file_path):
+#             creds = Credentials.from_authorized_user_file(token_file_path, SCOPES)   
     
-        if not creds or not creds.valid:
-            if creds and creds.expired and creds.refresh_token:
-                creds.refresh(Request())
+#         if not creds or not creds.valid:
+#             if creds and creds.expired and creds.refresh_token:
+#                 creds.refresh(Request())
                 
-            else:
-                credentials_file_path = os.path.join(secure_directory, 'credentials.json')
-                flow = InstalledAppFlow.from_client_secrets_file(credentials_file_path, SCOPES)
-                creds = flow.run_local_server(port=0)
+#             else:
+#                 credentials_file_path = os.path.join(secure_directory, 'credentials.json')
+#                 flow = InstalledAppFlow.from_client_secrets_file(credentials_file_path, SCOPES)
+#                 creds = flow.run_local_server(port=0)
     
-            with open(token_file_path, 'w') as token:
-                token.write(creds.to_json())
+#             with open(token_file_path, 'w') as token:
+#                 token.write(creds.to_json())
 
-        service = build('calendar', 'v3', credentials=creds)        
+#         service = build('calendar', 'v3', credentials=creds)        
         
-        paid_str = f'paid' if instance.paid else ''
-        reminder_str = f'!' if instance.reminder else ''
-        title = " ".join([reminder_str, instance.pickup_time, instance.flight_number, instance.flight_time, 'p'+str(instance.no_of_passenger), paid_str, '$'+instance.price, instance.contact])
-        address = " ".join([instance.street, instance.suburb])            
-        message = " ".join([instance.name, instance.email, 'b'+instance.no_of_baggage, 'm:'+instance.message, 'n:'+instance.notice, "d:"+str(instance.return_flight_date), '$'+instance.paid])            
-        flight_date = datetime.datetime.strptime(str(instance.flight_date), '%Y-%m-%d')
-        if instance.pickup_time:
-            pickup_time = datetime.datetime.strptime(instance.pickup_time, '%H:%M')
-        else:
-            pickup_time = datetime.datetime.strptime('00:00', '%H:%M') 
-        start = datetime.datetime.combine(flight_date, pickup_time.time())        
-        end = start + datetime.timedelta(hours=1)
+#         paid_str = f'paid' if instance.paid else ''
+#         reminder_str = f'!' if instance.reminder else ''
+#         title = " ".join([reminder_str, instance.pickup_time, instance.flight_number, instance.flight_time, 'p'+str(instance.no_of_passenger), paid_str, '$'+instance.price, instance.contact])
+#         address = " ".join([instance.street, instance.suburb])            
+#         message = " ".join([instance.name, instance.email, 'b'+instance.no_of_baggage, 'm:'+instance.message, 'n:'+instance.notice, "d:"+str(instance.return_flight_date), '$'+instance.paid])            
+#         flight_date = datetime.datetime.strptime(str(instance.flight_date), '%Y-%m-%d')
+#         if instance.pickup_time:
+#             pickup_time = datetime.datetime.strptime(instance.pickup_time, '%H:%M')
+#         else:
+#             pickup_time = datetime.datetime.strptime('00:00', '%H:%M') 
+#         start = datetime.datetime.combine(flight_date, pickup_time.time())        
+#         end = start + datetime.timedelta(hours=1)
 
-        event = {
-            'summary': title,
-            'location': address,
-            'start': {
-                'dateTime': start.strftime('%Y-%m-%dT%H:%M:%S'),
-                'timeZone': 'Australia/Sydney',
-            },
-            'end': {
-                'dateTime': end.strftime('%Y-%m-%dT%H:%M:%S'),
-                'timeZone': 'Australia/Sydney',
-            },
-            'description': message,
-        }    
+#         event = {
+#             'summary': title,
+#             'location': address,
+#             'start': {
+#                 'dateTime': start.strftime('%Y-%m-%dT%H:%M:%S'),
+#                 'timeZone': 'Australia/Sydney',
+#             },
+#             'end': {
+#                 'dateTime': end.strftime('%Y-%m-%dT%H:%M:%S'),
+#                 'timeZone': 'Australia/Sydney',
+#             },
+#             'description': message,
+#         }    
 
-        # Check if an event already exists for this instance
-        if instance.calendar_event_id:            
-            try:
-                event = service.events().update(calendarId='primary', eventId=instance.calendar_event_id, body=event).execute()
-                print('Event updated: %s' % (event.get('htmlLink')))
-            except HttpError as error:
-                print(f'An error occurred while updating the event: {error}')
-        else:
-            try:
-                event = service.events().insert(calendarId='primary', body=event).execute()
-                instance.calendar_event_id = event['id']  # Store the event ID in your model
-                instance.save()
-                print('Event created: %s' % (event.get('htmlLink')))
-            except HttpError as error:
-                print(f'An error occurred while creating the event: {error}')
+#         # Check if an event already exists for this instance
+#         if instance.calendar_event_id:            
+#             try:
+#                 event = service.events().update(calendarId='primary', eventId=instance.calendar_event_id, body=event).execute()
+#                 print('Event updated: %s' % (event.get('htmlLink')))
+#             except HttpError as error:
+#                 print(f'An error occurred while updating the event: {error}')
+#         else:
+#             try:
+#                 event = service.events().insert(calendarId='primary', body=event).execute()
+#                 instance.calendar_event_id = event['id']  # Store the event ID in your model
+#                 instance.save()
+#                 print('Event created: %s' % (event.get('htmlLink')))
+#             except HttpError as error:
+#                 print(f'An error occurred while creating the event: {error}')
                 
