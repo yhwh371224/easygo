12ab5e8ef77c84ee660836500d40377d7e7c287c
sung kam <sungkam3@gmail.com>
2023-12-07 14:31:30 +1100

    redis for caches

--- main/celery.py
+++ main/celery.py
@@ -1,7 +1,8 @@
+from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 from datetime import timedelta
-from celery.schedules import crontab
+# from celery.schedules import crontab
 
  
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
@@ -9,27 +10,29 @@ app = Celery('main')
 app.config_from_object('django.conf:settings', namespace='CELERY')
  
 app.conf.timezone = 'Australia/Sydney'
+
+app.autodiscover_tasks()
  
-app.conf.beat_schedule = {
-    "email_1": {
-        "task": "blog.tasks.email_1", 
-        "schedule": crontab(hour=0, minute=10)},
-    "email_2": {
-        "task": "blog.tasks.email_2", 
-        "schedule": crontab(hour=0, minute=10)},
-    "email_3": {
-        "task": "blog.tasks.email_3", 
-        "schedule": crontab(hour=0, minute=10)},
-    "email_4": {
-        "task": "blog.tasks.email_4", 
-        "schedule": crontab(hour=0, minute=10)},
-    "email_5": {
-        "task": "blog.tasks.email_5", 
-        "schedule": crontab(hour=0, minute=10)},
-    "email_6": {
-        "task": "blog.tasks.email_6", 
-        "schedule": crontab(hour=0, minute=10)},   
-    }
+# app.conf.beat_schedule = {
+#     "email_1": {
+#         "task": "blog.tasks.email_1", 
+#         "schedule": crontab(hour=0, minute=10)},
+#     "email_2": {
+#         "task": "blog.tasks.email_2", 
+#         "schedule": crontab(hour=0, minute=10)},
+#     "email_3": {
+#         "task": "blog.tasks.email_3", 
+#         "schedule": crontab(hour=0, minute=10)},
+#     "email_4": {
+#         "task": "blog.tasks.email_4", 
+#         "schedule": crontab(hour=0, minute=10)},
+#     "email_5": {
+#         "task": "blog.tasks.email_5", 
+#         "schedule": crontab(hour=0, minute=10)},
+#     "email_6": {
+#         "task": "blog.tasks.email_6", 
+#         "schedule": crontab(hour=0, minute=10)},   
+#     }
 
 # app.conf.beat_schedule = {
 #    "email_1": {
@@ -52,6 +55,6 @@ app.conf.beat_schedule = {
 #        "schedule": timedelta(seconds=60)},
 #    }
  
-app.autodiscover_tasks()
 
-app.loader.override_backends['django-db'] = 'django_celery_results.backends.database:DatabaseBackend'
+
+# app.loader.override_backends['django-db'] = 'django_celery_results.backends.database:DatabaseBackend'
