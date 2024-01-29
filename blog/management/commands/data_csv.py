373ce2981eb9b73fa75604c2cfc746ba90de24ba
sung kam <sungkam3@gmail.com>
2024-01-30 10:34:01 +1100

    html inquiry response updated

---
+++ blog/management/commands/data_csv.py
@@ -0,0 +1,43 @@
+import csv
+from blog.models import Inquiry, Post, Payment, Driver
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Exports data to CSV files'
+
+    def handle(self, *args, **kwargs):
+        # Inquiry
+        with open('inquiry_data.csv', 'w', newline='', encoding='utf-8') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow([f.name for f in Inquiry._meta.fields])  
+            for obj in Inquiry.objects.all():
+                writer.writerow([getattr(obj, f.name) for f in Inquiry._meta.fields])
+
+        # Post 
+        with open('post_data.csv', 'w', newline='', encoding='utf-8') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow([f.name for f in Post._meta.fields])  
+            for obj in Post.objects.all():
+                writer.writerow([getattr(obj, f.name) if f.name != 'driver_id' else obj.driver.driver_name if obj.driver else None for f in Post._meta.fields])
+
+        # Payment 
+        with open('payment_data.csv', 'w', newline='', encoding='utf-8') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow([f.name for f in Payment._meta.fields])  
+            for obj in Payment.objects.all():
+                writer.writerow([getattr(obj, f.name) for f in Payment._meta.fields])
+
+        # Driver 
+        with open('driver_data.csv', 'w', newline='', encoding='utf-8') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow([f.name for f in Driver._meta.fields])  
+            for obj in Driver.objects.all():
+                writer.writerow([getattr(obj, f.name) for f in Driver._meta.fields])
+
+
+
+
+
+
+
