47bc40349d8db30b49de3b082e54d0e1da870e65
sung kam <sungkam3@gmail.com>
2024-02-03 19:13:38 +1100

    feb 3 2004 1

--- blog/management/commands/data_csv.py
+++ blog/management/commands/data_csv.py
@@ -1,7 +1,7 @@
 import csv
 from blog.models import Inquiry, Post, Payment, Driver
 from django.core.management.base import BaseCommand
-
+from datetime import datetime
 
 class Command(BaseCommand):
     help = 'Exports data to CSV files'
@@ -14,12 +14,13 @@ class Command(BaseCommand):
             for obj in Inquiry.objects.all():
                 writer.writerow([getattr(obj, f.name) for f in Inquiry._meta.fields])
 
+
         # Post 
         with open('post_data.csv', 'w', newline='', encoding='utf-8') as csvfile:
             writer = csv.writer(csvfile)
             writer.writerow([f.name for f in Post._meta.fields])  
             for obj in Post.objects.all():
-                writer.writerow([getattr(obj, f.name) if f.name != 'driver_id' else obj.driver.driver_name if obj.driver else None for f in Post._meta.fields])
+                writer.writerow([getattr(obj, f.name) for f in Post._meta.fields])
 
         # Payment 
         with open('payment_data.csv', 'w', newline='', encoding='utf-8') as csvfile:
