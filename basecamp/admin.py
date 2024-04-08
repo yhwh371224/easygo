a67bc6e3d867bf3bd4051aec9266e898e3185cb1
sung kam <sungkam3@gmail.com>
2024-04-08 14:27:46 +1000

    basecamp/admin.py > email search

--- basecamp/admin.py
+++ basecamp/admin.py
@@ -8,7 +8,7 @@ class InquiryPointAdmin(admin.ModelAdmin):
     list_display = ['flight_date', 'contact', 'email', 'flight_number', 'street', 'pickup_time', 
                     'no_of_passenger', 'created']
 
-    search_fields = ['flight_date', 'pickup_time', 'flight_number',
+    search_fields = ['flight_date', 'pickup_time', 'flight_number', 'email',
                      'street', 'name', 'contact']
     
 
