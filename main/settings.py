d75baceb1437fbc9bdfdab37806a4f581ab89d9b
sung kam <sungkam3@gmail.com>
2024-04-11 12:26:12 +1000

    updated .env for right spelling

--- main/settings.py
+++ main/settings.py
@@ -150,14 +150,6 @@ DATABASES = {
 }
 
 
-# CACHES = {
-#     'default': {
-#         'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
-#         'LOCATION': '127.0.0.1:11211',
-#     }
-# }
-
-
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
