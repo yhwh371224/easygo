0da94893362b8c9a34b25997abf2b535c71de65a
sung kam <sungkam3@gmail.com>
2024-04-12 10:51:06 +1000

    confirm booking error fixed

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.admin import AdminSite
 from rangefilter.filters import DateRangeFilter
-from .models import Post, Inquiry, Payment, Driver
+from .models import Post, Inquiry, Payment, Driver, Inquiry_cruise, Inquiry_point
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -28,6 +28,22 @@ class InquiryAdmin(admin.ModelAdmin):
                      'name', 'contact']
     
 
+class InquiryPointAdmin(admin.ModelAdmin):
+    list_display = ['flight_date', 'contact', 'email', 'flight_number', 'street', 'pickup_time', 
+                    'no_of_passenger', 'created']
+
+    search_fields = ['flight_date', 'pickup_time', 'flight_number', 'email',
+                     'street', 'name', 'contact']
+
+
+class InquiryCruiseAdmin(admin.ModelAdmin):
+    list_display = ['flight_date', 'contact', 'email', 'flight_number', 'street', 'pickup_time', 
+                    'no_of_passenger', 'created']
+
+    search_fields = ['flight_date', 'pickup_time', 'flight_number', 'email',
+                     'street', 'name', 'contact']
+    
+
 class PaymentAdmin(admin.ModelAdmin):
     list_display = ['item_name', 'payer_email', 'gross_amount', 'txn_id', 'created']    
     
@@ -49,10 +65,14 @@ admin_site = MyAdminSite(name='horeb_yhwh')
 admin_site.register(Post, PostAdmin)
 admin_site.register(Inquiry, InquiryAdmin)
 admin_site.register(Payment, PaymentAdmin)
-admin_site.register(Driver, DriverAdmin)   
+admin_site.register(Driver, DriverAdmin)
+admin_site.register(Inquiry_point, InquiryPointAdmin)
+admin_site.register(Inquiry_cruise, InquiryCruiseAdmin)   
     
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Inquiry, InquiryAdmin)
 admin.site.register(Payment, PaymentAdmin)
 admin.site.register(Driver, DriverAdmin)
+admin.site.register(Inquiry_point, InquiryPointAdmin)
+admin.site.register(Inquiry_cruise, InquiryCruiseAdmin)
--- blog/models.py
+++ blog/models.py
@@ -5,14 +5,14 @@ from django.apps import AppConfig
 from django.urls import reverse
 
 
-class Inquiry(models.Model):
+class Inquiry_point(models.Model):
     name = models.CharField(max_length=100, blank=False)
     company_name = models.CharField(max_length=100, blank=True, null=True)
     contact = models.CharField(max_length=50, blank=False)
     email = models.EmailField(blank=False, db_index=True)
     email1 = models.EmailField(blank=True, null=True)
     flight_date = models.DateField(verbose_name='flight_date', blank=False)
-    flight_number = models.CharField(max_length=100, blank=False)
+    flight_number = models.CharField(max_length=100, blank=True, null=True)
     flight_time = models.CharField(max_length=30, blank=True, null=True)
     pickup_time = models.CharField(max_length=30, blank=True, null=True)
     direction = models.CharField(max_length=100, blank=True, null=True)
@@ -26,25 +26,109 @@ class Inquiry(models.Model):
     return_flight_time = models.CharField(max_length=30, blank=True, null=True)
     return_pickup_time = models.CharField(max_length=30, blank=True, null=True)
     message = models.TextField(blank=True, null=True)
-    notice = models.TextField(blank=True, null=True)    
-    price = models.CharField(max_length=100, blank=True, null=True)    
+    notice = models.TextField(blank=True, null=True)
+    price = models.CharField(max_length=100, blank=True, null=True)
     paid = models.CharField(max_length=30, blank=True, null=True)
+    discount = models.CharField(max_length=30, blank=True, null=True)
     driver = models.ForeignKey('Driver', on_delete=models.CASCADE, null=True, blank=True)
     meeting_point = models.CharField(max_length=100, blank=True, null=True)
-    is_confirmed = models.BooleanField(default=False, blank=True)        
-    cancelled = models.BooleanField(default=False, blank=True) 
-    private_ride = models.BooleanField(default=False, blank=True)  
-    cruise = models.BooleanField(default=False, blank=True)    
+    is_confirmed = models.BooleanField(default=False, blank=True)
+    cruise = models.BooleanField(default=False, blank=True)  
+    cancelled = models.BooleanField(default=False, blank=True)    
+    private_ride = models.BooleanField(default=False, blank=True)
+    reminder = models.BooleanField(default=False, blank=True)
+    sent_email = models.BooleanField(default=False, blank=True)
+    calendar_event_id = models.CharField(max_length=255, blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['-created']    
+
+
+class Inquiry_cruise(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    company_name = models.CharField(max_length=100, blank=True, null=True)
+    contact = models.CharField(max_length=50, blank=False)
+    email = models.EmailField(blank=False, db_index=True)
+    email1 = models.EmailField(blank=True, null=True)
+    flight_date = models.DateField(verbose_name='flight_date', blank=False)
+    flight_number = models.CharField(max_length=100, blank=True, null=True)
+    flight_time = models.CharField(max_length=30, blank=True, null=True)
+    pickup_time = models.CharField(max_length=30, blank=True, null=True)
+    direction = models.CharField(max_length=100, blank=True, null=True)
+    suburb = models.CharField(max_length=100, blank=True, null=True)
+    street = models.CharField(max_length=200, blank=True, null=True)
+    no_of_passenger = models.CharField(max_length=30, blank=False)
+    no_of_baggage = models.CharField(max_length=200, blank=True, null=True)
+    return_direction = models.CharField(max_length=100, blank=True, null=True)
+    return_flight_date = models.DateField(blank=True, null=True, default=datetime.date.today)
+    return_flight_number = models.CharField(max_length=200, blank=True, null=True)
+    return_flight_time = models.CharField(max_length=30, blank=True, null=True)
+    return_pickup_time = models.CharField(max_length=30, blank=True, null=True)
+    message = models.TextField(blank=True, null=True)
+    notice = models.TextField(blank=True, null=True)
+    price = models.CharField(max_length=100, blank=True, null=True)
+    paid = models.CharField(max_length=30, blank=True, null=True)
+    discount = models.CharField(max_length=30, blank=True, null=True)
+    driver = models.ForeignKey('Driver', on_delete=models.CASCADE, null=True, blank=True)
+    meeting_point = models.CharField(max_length=100, blank=True, null=True)
+    is_confirmed = models.BooleanField(default=False, blank=True)
+    cruise = models.BooleanField(default=False, blank=True)  
+    cancelled = models.BooleanField(default=False, blank=True)    
+    private_ride = models.BooleanField(default=False, blank=True)
+    reminder = models.BooleanField(default=False, blank=True)
+    sent_email = models.BooleanField(default=False, blank=True)
+    calendar_event_id = models.CharField(max_length=255, blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created']    
+
+
+class Inquiry(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    company_name = models.CharField(max_length=100, blank=True, null=True)
+    contact = models.CharField(max_length=50, blank=False)
+    email = models.EmailField(blank=False, db_index=True)
+    email1 = models.EmailField(blank=True, null=True)
+    flight_date = models.DateField(verbose_name='flight_date', blank=False)
+    flight_number = models.CharField(max_length=100, blank=True, null=True)
+    flight_time = models.CharField(max_length=30, blank=True, null=True)
+    pickup_time = models.CharField(max_length=30, blank=True, null=True)
+    direction = models.CharField(max_length=100, blank=True, null=True)
+    suburb = models.CharField(max_length=100, blank=True, null=True)
+    street = models.CharField(max_length=200, blank=True, null=True)
+    no_of_passenger = models.CharField(max_length=30, blank=False)
+    no_of_baggage = models.CharField(max_length=200, blank=True, null=True)
+    return_direction = models.CharField(max_length=100, blank=True, null=True)
+    return_flight_date = models.DateField(blank=True, null=True, default=datetime.date.today)
+    return_flight_number = models.CharField(max_length=200, blank=True, null=True)
+    return_flight_time = models.CharField(max_length=30, blank=True, null=True)
+    return_pickup_time = models.CharField(max_length=30, blank=True, null=True)
+    message = models.TextField(blank=True, null=True)
+    notice = models.TextField(blank=True, null=True)
+    price = models.CharField(max_length=100, blank=True, null=True)
+    paid = models.CharField(max_length=30, blank=True, null=True)
+    discount = models.CharField(max_length=30, blank=True, null=True)
+    driver = models.ForeignKey('Driver', on_delete=models.CASCADE, null=True, blank=True)
+    meeting_point = models.CharField(max_length=100, blank=True, null=True)
+    is_confirmed = models.BooleanField(default=False, blank=True)
+    cruise = models.BooleanField(default=False, blank=True)  
+    cancelled = models.BooleanField(default=False, blank=True)    
+    private_ride = models.BooleanField(default=False, blank=True)
+    reminder = models.BooleanField(default=False, blank=True)
+    sent_email = models.BooleanField(default=False, blank=True)
+    calendar_event_id = models.CharField(max_length=255, blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:        
         ordering = ['-created']
     
-    def get_absolute_url(self):
-        return '/blog/inquiry{}/'.format(self.pk)
+    # def get_absolute_url(self):
+    #     return '/blog/inquiry{}/'.format(self.pk)
 
-    def get_confirm_url(self):
-        return reverse('confirm_reminder', kwargs={'email': self.email})
+    # def get_confirm_url(self):
+    #     return reverse('confirm_reminder', kwargs={'email': self.email})
     
         
 class Post(models.Model):
@@ -86,11 +170,11 @@ class Post(models.Model):
     class Meta:
         ordering = ['-created']
 
-    def get_absolute_url(self):
-        return '/blog/{}/'.format(self.pk)
+    # def get_absolute_url(self):
+    #     return '/blog/{}/'.format(self.pk)
 
-    def get_update_url(self):
-        return self.get_absolute_url() + 'update/'
+    # def get_update_url(self):
+    #     return self.get_absolute_url() + 'update/'
     
 
 class Payment(models.Model):
