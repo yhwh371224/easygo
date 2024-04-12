540537f3c44fb9dd5695503998ad553bac37e27c
sung kam <sungkam3@gmail.com>
2024-04-12 12:47:44 +1000

    blog.models.py and blog.admin.py sorted by order

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,28 @@ from django.apps import AppConfig
 from django.urls import reverse
 
 
-class Inquiry_point(models.Model):
+
+class BlogAppConfig(AppConfig):
+    name = 'blog'
+
+    def ready(self):
+        from . import signals
+
+
+class Driver(models.Model):
+    driver_name = models.CharField(max_length=100, blank=True, null=True)
+    driver_contact = models.CharField(max_length=50, blank=True, null=True)
+    driver_email = models.EmailField(blank=True, null=True)
+    driver_address = models.CharField(max_length=200, blank=True, null=True)
+    driver_plate = models.CharField(max_length=30, blank=True, null=True)
+    driver_car = models.CharField(max_length=30, blank=True, null=True)
+    driver_bankdetails = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.driver_name
+
+
+class Inquiry(models.Model):
     name = models.CharField(max_length=100, blank=False)
     company_name = models.CharField(max_length=100, blank=True, null=True)
     contact = models.CharField(max_length=50, blank=False)
@@ -40,9 +61,9 @@ class Inquiry_point(models.Model):
     sent_email = models.BooleanField(default=False, blank=True)
     calendar_event_id = models.CharField(max_length=255, blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
-    
-    class Meta:
-        ordering = ['-created']    
+
+    class Meta:        
+        ordering = ['-created'] 
 
 
 class Inquiry_cruise(models.Model):
@@ -85,7 +106,7 @@ class Inquiry_cruise(models.Model):
         ordering = ['-created']    
 
 
-class Inquiry(models.Model):
+class Inquiry_point(models.Model):
     name = models.CharField(max_length=100, blank=False)
     company_name = models.CharField(max_length=100, blank=True, null=True)
     contact = models.CharField(max_length=50, blank=False)
@@ -120,16 +141,21 @@ class Inquiry(models.Model):
     sent_email = models.BooleanField(default=False, blank=True)
     calendar_event_id = models.CharField(max_length=255, blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['-created'] 
+        
 
-    class Meta:        
+class Payment(models.Model):
+    item_name = models.CharField(max_length=100, blank=True, null=True)
+    payer_email = models.EmailField(blank=True, null=True)
+    gross_amount = models.CharField(max_length=30, blank=True, null=True)
+    txn_id = models.CharField(max_length=30, blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
         ordering = ['-created']
-    
-    # def get_absolute_url(self):
-    #     return '/blog/inquiry{}/'.format(self.pk)
 
-    # def get_confirm_url(self):
-    #     return reverse('confirm_reminder', kwargs={'email': self.email})
-    
         
 class Post(models.Model):
     name = models.CharField(max_length=100, blank=False)
@@ -168,43 +194,7 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ['-created']
+        ordering = ['-created'] 
 
-    # def get_absolute_url(self):
-    #     return '/blog/{}/'.format(self.pk)
-
-    # def get_update_url(self):
-    #     return self.get_absolute_url() + 'update/'
-    
-
-class Payment(models.Model):
-    item_name = models.CharField(max_length=100, blank=True, null=True)
-    payer_email = models.EmailField(blank=True, null=True)
-    gross_amount = models.CharField(max_length=30, blank=True, null=True)
-    txn_id = models.CharField(max_length=30, blank=True, null=True)
-    created = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        ordering = ['-created']
-
-
-class Driver(models.Model):
-    driver_name = models.CharField(max_length=100, blank=True, null=True)
-    driver_contact = models.CharField(max_length=50, blank=True, null=True)
-    driver_email = models.EmailField(blank=True, null=True)
-    driver_address = models.CharField(max_length=200, blank=True, null=True)
-    driver_plate = models.CharField(max_length=30, blank=True, null=True)
-    driver_car = models.CharField(max_length=30, blank=True, null=True)
-    driver_bankdetails = models.TextField(blank=True, null=True)
-
-    def __str__(self):
-        return self.driver_name 
-    
-
-class BlogAppConfig(AppConfig):
-    name = 'blog'
-
-    def ready(self):
-        from . import signals
 
 
