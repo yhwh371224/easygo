e73fc57727e8985458a0be4239d34b7e52ca1365
sung kam <sungkam3@gmail.com>
2024-04-13 20:05:44 +1000

    basecamp.models and admin.py for p2p multiple invoice

--- basecamp/admin.py
+++ basecamp/admin.py
@@ -1,15 +1,13 @@
-# from django.contrib import admin
-# from .models import Inquiry_point, Inquiry_cruise
-# from rangefilter.filters import DateRangeFilter
-# from blog.admin import admin_site
+from django.contrib import admin
+from .models import Booking_p2p
+from rangefilter.filters import DateRangeFilter
+from blog.admin import admin_site
 
 
-# class InquiryPointAdmin(admin.ModelAdmin):
-#     list_display = ['flight_date', 'contact', 'email', 'flight_number', 'street', 'pickup_time', 
-#                     'no_of_passenger', 'created']
+class BookingP2pAdmin(admin.ModelAdmin):
+    list_display = ['p2p_date', 'p2p_phone', 'p2p_name', 'first_pickup_location', 'pickup_time', 'created']
 
-#     search_fields = ['flight_date', 'pickup_time', 'flight_number', 'email',
-#                      'street', 'name', 'contact']
+    search_fields = ['p2p_date', 'first_putime', 'p2p_email', 'p2p_name', 'p2p_phone']
 
 
 # class InquiryCruiseAdmin(admin.ModelAdmin):
@@ -20,10 +18,8 @@
 #                      'street', 'name', 'contact']
     
 
-# admin_site.register(Inquiry_point, InquiryPointAdmin)
-# admin_site.register(Inquiry_cruise, InquiryCruiseAdmin)
+admin_site.register(Booking_p2p, BookingP2pAdmin)
 
-# admin.site.register(Inquiry_point, InquiryPointAdmin)
-# admin.site.register(Inquiry_cruise, InquiryCruiseAdmin)
+admin.site.register(Booking_p2p, BookingP2pAdmin)
 
 
