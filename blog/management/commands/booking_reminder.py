a2fe0c845f603da3224c6f4892c2f2d3437f65ec
sung kam <sungkam3@gmail.com>
2024-05-14 18:32:27 +1000

    booking_reminder updated

--- blog/management/commands/booking_reminder.py
+++ blog/management/commands/booking_reminder.py
@@ -8,8 +8,6 @@ from django.utils.html import strip_tags
 from blog.models import Post
 from main.settings import RECIPIENT_EMAIL
 
-
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 class Command(BaseCommand):
@@ -25,15 +23,14 @@ class Command(BaseCommand):
 
     def send_email(self, date_offset, template_name, subject):
         target_date = date.today() + timedelta(days=date_offset)
-        booking_reminders = Post.objects.filter(flight_date=target_date)
+        booking_reminders = Post.objects.filter(flight_date=target_date, cancelled=False)
         self.send_email_task(booking_reminders, template_name, subject)
 
     def send_email_task(self, booking_reminders, template_name, subject):
-        # sent_emails_set = set()  # Set to track already sent emails
-        
+                
         for booking_reminder in booking_reminders:
-
-            driver_instance = booking_reminder.driver       
+            driver_instance = booking_reminder.driver
+            driver_name, driver_contact, driver_plate, driver_car = "", "", "", ""   
 
             if driver_instance:
                 driver_name = driver_instance.driver_name
@@ -41,40 +38,28 @@ class Command(BaseCommand):
                 driver_plate = driver_instance.driver_plate
                 driver_car = driver_instance.driver_car
 
-                html_content = render_to_string(template_name, {
-                    'name': booking_reminder.name, 'flight_date': booking_reminder.flight_date, 'flight_number': booking_reminder.flight_number,
-                    'flight_time': booking_reminder.flight_time, 'direction': booking_reminder.direction, 'pickup_time': booking_reminder.pickup_time,
-                    'street': booking_reminder.street, 'suburb': booking_reminder.suburb, 'price': booking_reminder.price, 'meeting_point': booking_reminder.meeting_point,
-                    'driver_name': driver_name, 'driver_contact': driver_contact, 'driver_plate': driver_plate, 'driver_car': driver_car, 
-                    'paid': booking_reminder.paid
-                })
-                
-            else: 
-                html_content = render_to_string(template_name, {
-                    'name': booking_reminder.name, 'flight_date': booking_reminder.flight_date, 'flight_number': booking_reminder.flight_number,
-                    'flight_time': booking_reminder.flight_time, 'direction': booking_reminder.direction, 'pickup_time': booking_reminder.pickup_time,
-                    'street': booking_reminder.street, 'suburb': booking_reminder.suburb, 'price': booking_reminder.price, 'meeting_point': booking_reminder.meeting_point,
-                    'paid': booking_reminder.paid
-                })
+            html_content = render_to_string(template_name, {
+                'name': booking_reminder.name, 'flight_date': booking_reminder.flight_date, 'flight_number': booking_reminder.flight_number,
+                'flight_time': booking_reminder.flight_time, 'direction': booking_reminder.direction, 'pickup_time': booking_reminder.pickup_time,
+                'street': booking_reminder.street, 'suburb': booking_reminder.suburb, 'price': booking_reminder.price, 'meeting_point': booking_reminder.meeting_point,
+                'driver_name': driver_name, 'driver_contact': driver_contact, 'driver_plate': driver_plate, 'driver_car': driver_car, 
+                'paid': booking_reminder.paid, 'cash': booking_reminder.cash
+            })
 
-            text_content = strip_tags(html_content)
+            text_content = strip_tags(html_content)            
             email = EmailMultiAlternatives(subject, text_content, '', [booking_reminder.email])
             email.attach_alternative(html_content, "text/html")
-            email.send() 
-            booking_reminder.save()
-            # sent_emails_set.add(booking_reminder.email)
-
-            if booking_reminder.cancelled: 
-                # or booking_reminder.email in sent_emails_set:
-                continue 
+            try:
+                email.send()
+                booking_reminder.save()
+            except Exception as e:
+                print(f"Failed to send email to {booking_reminder.email}: {e}")
 
             if not booking_reminder.calendar_event_id:
                 subject = "No confirmation yet - empty id"
                 message = f"{booking_reminder.name} & {booking_reminder.email}"
-                recipient_list = [RECIPIENT_EMAIL]
-                send_mail(subject, message, '', recipient_list)
-
-            
-
-                   
-                    
+                recipient = [RECIPIENT_EMAIL]
+                try:
+                    send_mail(subject, message, '', recipient)
+                except Exception as e:
+                    print(f"Failed to send confirmation email to {booking_reminder.email}: {e}")
