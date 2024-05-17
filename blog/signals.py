5a4b383e39c8c7f8fae962c6dd463061b0e491cf
sung kam <sungkam3@gmail.com>
2024-05-17 20:08:22 +1000

    signals.py and tasks.py for inquiry

--- blog/signals.py
+++ blog/signals.py
@@ -9,8 +9,7 @@ from django.dispatch import receiver
 
 from main.settings import RECIPIENT_EMAIL
 from .models import Post, Inquiry, Payment, Inquiry_point, Inquiry_cruise
-from .tasks import create_event_on_calendar, send_inquiry_confirmed_email, send_inquiry_cancelled_email
-# from basecamp.models import Inquiry_point, Inquiry_cruise
+from .tasks import create_event_on_calendar
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 
@@ -34,57 +33,40 @@ def notify_user_post(sender, instance, created, **kwargs):
 # Flight inquiry
 @receiver(post_save, sender=Inquiry)
 def notify_user_inquiry(sender, instance, created, **kwargs):
-    if instance.is_confirmed: 
-        send_inquiry_confirmed_email.delay(instance_data={
-            'company_name': instance.company_name,
-            'name': instance.name,
-            'contact': instance.contact,
-            'email': instance.email,
-            'flight_date': instance.flight_date,
-            'flight_number': instance.flight_number,
-            'flight_time': instance.flight_time,
-            'pickup_time': instance.pickup_time,
-            'direction': instance.direction,
-            'street': instance.street,
-            'suburb': instance.suburb,
-            'no_of_passenger': instance.no_of_passenger,
-            'no_of_baggage': instance.no_of_baggage,
-            'return_direction': instance.return_direction,
-            'return_flight_date': instance.return_flight_date,
-            'return_flight_number': instance.return_flight_number,
-            'return_flight_time': instance.return_flight_time,
-            'return_pickup_time': instance.return_pickup_time,
-            'message': instance.message,
-            'price': instance.price,
-            'notice': instance.notice,
-            'private_ride': instance.private_ride,
-        })
+    if instance.is_confirmed:
+        html_content = render_to_string("basecamp/html_email-inquiry-response.html",
+                                        {'company_name': instance.company_name, 'name': instance.name, 'contact': instance.contact, 
+                                         'email': instance.email, 'flight_date': instance.flight_date, 'flight_number': instance.flight_number,
+                                         'flight_time': instance.flight_time, 'pickup_time': instance.pickup_time, 'direction': instance.direction, 
+                                         'street': instance.street, 'suburb': instance.suburb, 'no_of_passenger': instance.no_of_passenger,
+                                         'no_of_baggage': instance.no_of_baggage, 'return_direction': instance.return_direction, 
+                                         'return_flight_date': instance.return_flight_date, 'return_flight_number': instance.return_flight_number, 
+                                         'return_flight_time': instance.return_flight_time, 'return_pickup_time': instance.return_pickup_time, 
+                                         'message': instance.message, 'price': instance.price, 'notice': instance.notice, 'private_ride': instance.private_ride,})
+        text_content = strip_tags(html_content)
+        email = EmailMultiAlternatives(
+            "EasyGo Booking Inquiry",
+            text_content,
+            '',
+            [instance.email, RECIPIENT_EMAIL]
+        )
+        email.attach_alternative(html_content, "text/html")
+        email.send()         
 
     elif instance.cancelled:
-        send_inquiry_cancelled_email.delay(instance_data={
-            'company_name': instance.company_name,
-            'name': instance.name,
-            'contact': instance.contact,
-            'email': instance.email,
-            'flight_date': instance.flight_date,
-            'flight_number': instance.flight_number,
-            'flight_time': instance.flight_time,
-            'pickup_time': instance.pickup_time,
-            'direction': instance.direction,
-            'street': instance.street,
-            'suburb': instance.suburb,
-            'no_of_passenger': instance.no_of_passenger,
-            'no_of_baggage': instance.no_of_baggage,
-            'return_direction': instance.return_direction,
-            'return_flight_date': instance.return_flight_date,
-            'return_flight_number': instance.return_flight_number,
-            'return_flight_time': instance.return_flight_time,
-            'return_pickup_time': instance.return_pickup_time,
-            'message': instance.message,
-            'price': instance.price,
-            'notice': instance.notice,
-            'private_ride': instance.private_ride,
-        })
+        html_content = render_to_string("basecamp/html_email-cancelled.html",
+                                        {'name': instance.name, 'email': instance.email,
+                                         })
+        text_content = strip_tags(html_content)
+        email = EmailMultiAlternatives(
+            "EasyGo Booking Inquiry",
+            text_content,
+            '',
+            [RECIPIENT_EMAIL, instance.email]
+        )
+        email.attach_alternative(html_content, "text/html")
+        email.send()
+        
 
 
 # Point to point inquiry
