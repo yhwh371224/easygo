a5863d52b9043b162dcd99ea73a65559b88c2177
sung kam <sungkam3@gmail.com>
2024-05-25 14:34:40 +1000

    lottery updated and new one

--- lottery/oz_lotto.py
+++ lottery/oz_lotto.py
@@ -1,21 +1,30 @@
-from random import *
+import time
+import random
 
-print('How many games would you like to pick?')
+random.seed(time.time())
 
+print("How many total numbers you like to choose?")
+c = input()
+print('')
+
+# 24 numbers
+numbers = list(range(1, 48))
+numbers_picked = random.sample(numbers, int(c))
+numbers_picked.sort()
+print(numbers_picked)
+print('')
+
+numbers_shuffled = random.shuffle(numbers_picked)
+
+print('How many games would you like to play?')
 n = input()
-
 print('')
 
-lotto_number = list(range(1, 48))
-shuffle(lotto_number)
-
-print('Oz Lotto numbers')
+print('Please find your numbers below, Good luck!')
 print('')
+
 for i in range(int(n)):
-    shuffle(lotto_number)
-    winner_numbers = sample(lotto_number, 7)
-    winner_numbers.sort()    
+    winner_numbers = random.sample(numbers_shuffled, 7)
+    winner_numbers.sort()
     print(f"{i+1}회: {winner_numbers}")
     print('')
-
-
