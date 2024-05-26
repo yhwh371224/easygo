194635e58dea51caf8a353c2132d97bba90892b7
sung kam <sungkam3@gmail.com>
2024-05-26 21:05:30 +1000

    easygo_review admin.py updated

--- easygo_review/admin.py
+++ easygo_review/admin.py
@@ -1,7 +1,27 @@
 from django.contrib import admin
+from django.contrib.admin import AdminSite
+from rangefilter.filters import DateRangeFilter
 from .models import Post, Comment
 
 
-admin.site.register(Post)
-admin.site.register(Comment)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['date', 'name', 'is_published', 'created']
+    list_filter = (('date', DateRangeFilter), 'name')
+    search_fields = ['date', 'name']
 
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['post', 'author', 'created_at', 'modified_at']
+    search_fields = ['author', 'created_at', 'modified_at']
+
+
+class MyAdminSite(AdminSite):
+    site_header = 'EasyGo administration'
+
+admin_site = MyAdminSite(name='horeb_yhwh')
+admin_site.register(Post, PostAdmin)
+admin_site.register(Comment, CommentAdmin)
+
+
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Post, PostAdmin)
