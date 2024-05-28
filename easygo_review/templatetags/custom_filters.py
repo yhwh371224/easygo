04b7c1d521b0fdbe0d2b6e22493fed92cc12d05f
sungkam718 <sungkam718@gmail.com>
2024-05-28 22:11:30 +1000

    easygo_review: filter range used and fixed tiny code

--- easygo_review/templatetags/custom_filters.py
+++ easygo_review/templatetags/custom_filters.py
@@ -5,3 +5,7 @@ register = template.Library()
 @register.filter
 def filter_range(value):
     return range(int(value))
+
+@register.filter
+def subtract_from_five(value):
+    return range(5 - int(value))
