3588f3728c130564c1ecbdb9332623a78cd0043b
sung kam <sungkam3@gmail.com>
2024-05-31 22:51:10 +1000

    tiny updated on lottery

--- lottery/views.py
+++ lottery/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 import random
+import time
 
 
 def lotto_form(request):
     return render(request, 'lottery/lotto_form.html')
 
 def lotto_result(request):
+    random.seed(time.time())
     c = int(request.POST['total_numbers'])
     n = int(request.POST['games'])
     
@@ -15,6 +17,7 @@ def lotto_result(request):
 
     results = []
     for i in range(n):
+        random.shuffle(numbers_picked)
         winner_numbers = random.sample(numbers_picked, 6)
         winner_numbers.sort()
         results.append(f"{i+1}회: {winner_numbers}")
