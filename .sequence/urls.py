7213ad81c59fe44dfe0edee3d172c1729ef14385
sung kam <sungkam3@gmail.com>
2024-06-04 10:37:41 +1000

    removed sequence from eveywhere

--- .sequence/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('', views.sequence_form, name='sequence_form'),
-    path('result/', views.sequence_result, name='sequence_result'),
-]
