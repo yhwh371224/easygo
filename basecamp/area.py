ed105e84ee66e5ddab0cd1832c0bce268edc018f
sung kam <sungkam3@gmail.com>
2024-06-10 15:11:45 +1000

    more_suburbs: alert, area.py, custom_login, reviews on navbar

--- basecamp/area.py
+++ basecamp/area.py
@@ -12,30 +12,30 @@ def get_suburbs():
             'description': 'Easygo Airport Shuttle provide a private/shared ride for two persons: $85, for three persons: $95',
             'extra': '*additional $10 per person*'
         },                
-        'St Ives': {
-            'price': 90,
-            'image_url': '/static/basecamp/photos/st-ives.webp',
-            'description': 'EasyGo Airport Shuttle provide a private/shared ride for two persons: $100, for three persons: $110',
-            'extra': '*additional $10 per person*'
-        },
-        'Ryde': {
-            'price': 75,
-            'image_url': '/static/basecamp/photos/ryde.webp',
-            'description': 'EasyGo Airport Shuttle provide a private/shared ride for two persons: $85, for three persons: $95',
-            'extra': '*additional $10 per person*'
-        }, 
+        # 'St Ives': {
+        #     'price': 90,
+        #     'image_url': '/static/basecamp/photos/st-ives.webp',
+        #     'description': 'EasyGo Airport Shuttle provide a private/shared ride for two persons: $100, for three persons: $110',
+        #     'extra': '*additional $10 per person*'
+        # },
+        # 'Ryde': {
+        #     'price': 75,
+        #     'image_url': '/static/basecamp/photos/ryde.webp',
+        #     'description': 'EasyGo Airport Shuttle provide a private/shared ride for two persons: $85, for three persons: $95',
+        #     'extra': '*additional $10 per person*'
+        # }, 
         'Hornsby': {
             'price': 110,
             'image_url': '/static/basecamp/photos/hornsby.webp',
             'description': 'EasyGo Airport Shuttle provide a private/shared ride for two persons: $120, for three persons: $130',
             'extra': '*additional $10 per person*'
         },
-        'Blacktown': {
-            'price': 120,
-            'image_url': '/static/basecamp/photos/blacktown.webp',
-            'description': 'EasyGo Airport Shuttle provide a private/shared ride for two persons: $130, for three persons: $140',
-            'extra': '*additional $10 per person*'
-        },    
+        # 'Blacktown': {
+        #     'price': 120,
+        #     'image_url': '/static/basecamp/photos/blacktown.webp',
+        #     'description': 'EasyGo Airport Shuttle provide a private/shared ride for two persons: $130, for three persons: $140',
+        #     'extra': '*additional $10 per person*'
+        # },    
     }
     return suburbs
 
