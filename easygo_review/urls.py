d49a3669ef50ba4927b0049b7a5e677671eae74a
sungk <sungk@gmail.com>
2024-06-12 16:58:15 +1000

    email only loging now but AnoymousUser

--- easygo_review/urls.py
+++ easygo_review/urls.py
@@ -5,15 +5,16 @@ from django.contrib.auth import views as auth_views
 app_name = 'easygo_review'
 
 urlpatterns = [
-    path('search/<str:q>/', views.PostSearch.as_view()),
-    path('edit_comment/<int:pk>/', views.CommentUpdate.as_view()),
-    path('delete_comment/<int:pk>/', views.delete_comment),
+    path('search/<str:q>/', views.PostSearch.as_view(), name="post_search"),
+    path('edit_comment/<int:pk>/', views.CommentUpdate.as_view(), name='edit_comment'),
+    path('delete_comment/<int:pk>/', views.delete_comment, name='delete_comment'),
     path('<int:pk>/new_comment/', views.new_comment, name="easygo_review/<pk>/new_comment/"),
     path('<int:pk>/update/', views.PostUpdate.as_view(), name="easygo_review/<pk>/update/"),
     path('<int:pk>/', views.PostDetail.as_view(), name="easygo_review/<pk>"),
     path('create/', views.PostCreate.as_view(), name="easygo_review/create"),
     path('', views.PostList.as_view(), name="easygo_review"),
-    path('custom_login/', auth_views.LoginView.as_view(template_name='easygo_review/custom_login.html'), name='custom_login'),
+    path('custom_login/', views.custom_login_view, name='custom_login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='easygo_review'), name='logout'),
 
 ]
 
