0ba002b6980cab2e07ae14f62378ae3a124cf575
sungk <sungk@gmail.com>
2024-06-13 05:31:09 +1000

    google review recovered and reinstated

--- blog/management/commands/booking_reminder.py
+++ blog/management/commands/booking_reminder.py
@@ -35,14 +35,14 @@ class Command(BaseCommand):
         return logger
 
     def handle(self, *args, **options):
-        reminder_intervals = [0, 1, 3, 7, 14]
+        reminder_intervals = [0, 1, 3, 7, 14, -1]
         templates = [
             "basecamp/html_email-today.html",
             "basecamp/html_email-tomorrow.html",
             "basecamp/html_email-upcoming3.html",
             "basecamp/html_email-upcoming7.html",
             "basecamp/html_email-upcoming14.html",
-            # "basecamp/html_email-yesterday.html",
+            "basecamp/html_email-yesterday.html",
         ]
         subjects = [
             "Reminder-Today",
@@ -50,7 +50,7 @@ class Command(BaseCommand):
             "Reminder-3days",
             "Reminder-7days",
             "Reminder-2wks",
-            # "Review-EasyGo",
+            "Review-EasyGo",
         ]
         for interval, template, subject in zip(reminder_intervals, templates, subjects):
             self.send_email(interval, template, subject)
