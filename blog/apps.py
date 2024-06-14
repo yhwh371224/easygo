b1508c9877106181ea55094b70cf2d0663cd5c22
sungk <sungk@gmail.com>
2024-06-14 22:45:28 +1000

    fixed some problem, admin.py & pip install stripe:done

--- blog/apps.py
+++ blog/apps.py
@@ -5,10 +5,8 @@ class BlogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'blog'
 
-    def ready(self):
-        # from django.db.backends.signals import connection_created        
-        # from blog import tasks        
+    def ready(self): 
         import blog.signals        
-        # connection_created.connect(tasks.handle_database_change)
+
 
 
