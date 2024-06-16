0009ab2fb8d019967552169966630d3ba6f44f5d
sung kam <sungkam3@gmail.com>
2024-06-16 22:53:46 +1000

    stripe is working but 3% not yet

--- blog/models.py
+++ blog/models.py
@@ -164,13 +164,16 @@ class PayPalPayment(models.Model):
 
 
 class StripePayment(models.Model):
-    item_name = models.CharField(max_length=100, blank=True, null=True)
+    user_name = models.CharField(max_length=100, blank=True, null=True)
     customer_email = models.EmailField(blank=True, null=True)
     amount_total = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ['-created']
+
+    def __str__(self):
+        return f"{self.user_name} - {self.amount_total}"
 
         
 class Post(models.Model):
