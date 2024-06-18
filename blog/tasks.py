98ee6419573ce84fa7c5ce75d51ad97ea643c4f4
sungkam718 <sungkam718@gmail.com>
2024-06-18 13:41:07 +1000

    stripe tasks.py is now working

--- blog/tasks.py
+++ blog/tasks.py
@@ -1,7 +1,6 @@
 import datetime
 import os
 import re
-import logging
 
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
@@ -24,7 +23,7 @@ def create_event_on_calendar(instance_id):
 
     SCOPES = ['https://www.googleapis.com/auth/calendar']
     SERVICE_ACCOUNT_FILE = 'secure/calendar/calendar-service-account-file.json'
-    DELEGATED_USER_EMAIL = RECIPIENT_EMAIL  # 위임받은 사용자의 이메일 주소
+    DELEGATED_USER_EMAIL = RECIPIENT_EMAIL  
 
     credentials = service_account.Credentials.from_service_account_file(
         SERVICE_ACCOUNT_FILE, scopes=SCOPES, subject=DELEGATED_USER_EMAIL)
@@ -160,7 +159,7 @@ def payment_send_email(subject, html_content, recipient_list):
 
 
 @shared_task
-def notify_user_payment_paypal(instance_id):    
+def notify_user_payment_paypal(instance_id):
     instance = PayPalPayment.objects.get(id=instance_id)
     if instance.item_name:
         post_name = Post.objects.filter(
@@ -200,51 +199,41 @@ def notify_user_payment_paypal(instance_id):
             payment_send_email("Payment - EasyGo", html_content, [instance.payer_email, RECIPIENT_EMAIL])
 
 
-logger = logging.getLogger(__name__)
-
-@shared_task
+# Stripe > sending email & save
 def notify_user_payment_stripe(instance_id):
-    logger.info(f"Starting notify_user_payment_stripe task for instance ID: {instance_id}")
-    try:
-        instance = StripePayment.objects.get(id=instance_id)
-        if instance.name:
-            post_name = Post.objects.filter(
-                Q(name__iregex=r'^%s$' % re.escape(instance.name)) |
-                Q(email__iexact=instance.email)
-            ).first()
+    instance = StripePayment.objects.get(id=instance_id)   
+    if instance.name:            
+        post_name = Post.objects.filter(
+            Q(name__iregex=r'^%s$' % re.escape(instance.name)) |
+            Q(email__iexact=instance.email)
+        ).first()
 
-            if post_name:
-                amount = str(instance.amount)
-                html_content = render_to_string(
-                    "basecamp/html_email-payment-success-stripe.html",
-                    {'name': post_name.name, 'email': post_name.email, 'amount': amount}
-                )
-                payment_send_email("Payment - EasyGo", html_content, [post_name.email, RECIPIENT_EMAIL])
-                
-                checking_message = "short payment"
-                post_name.paid = amount          
-                post_name.reminder = True
-                post_name.discount = ""
-                if float(post_name.price) > instance.amount:
-                    post_name.toll = checking_message             
-                    post_name.save()
+        if post_name:            
+            html_content = render_to_string(
+                "basecamp/html_email-payment-success-stripe.html",
+                {'name': post_name.name, 'email': post_name.email, 'amount': instance.amount}
+            )
+            payment_send_email("Payment - EasyGo", html_content, [post_name.email, RECIPIENT_EMAIL])            
+            
+            post_name.paid = instance.amount          
+            post_name.reminder = True
+            post_name.discount = ""
+            if float(post_name.price) > instance.amount:
+                post_name.toll = "short payment"             
+                post_name.save()
 
-                if post_name.return_pickup_time == 'x':                   
-                        second_post = Post.objects.filter(email=post_name.email)[1]                    
-                        second_post.paid = amount                    
-                        second_post.reminder = True
-                        second_post.discount = ""
-                        if float(post_name.price) > instance.amount:
-                            second_post.toll = checking_message 
-                            second_post.save() 
+            if post_name.return_pickup_time == 'x':                   
+                    second_post = Post.objects.filter(email=post_name.email)[1]                    
+                    second_post.paid = instance.amount                    
+                    second_post.reminder = True
+                    second_post.discount = ""
+                    if float(post_name.price) > instance.amount:
+                        second_post.toll = "short payment"  
+                        second_post.save() 
 
-            else:
-                amount = str(instance.amount)
-                html_content = render_to_string(
-                    "basecamp/html_email-noIdentity-stripe.html",
-                    {'name': instance.name, 'email': instance.email, 'amount': amount}
-                )
-                payment_send_email("Payment - EasyGo", html_content, [instance.email, RECIPIENT_EMAIL])
-        logger.info(f"Completed notify_user_payment_stripe task for instance ID: {instance_id}")
-    except Exception as e:
-        logger.error(f"Error in notify_user_payment_stripe task for instance ID: {instance_id}: {e}")
+        else:            
+            html_content = render_to_string(
+                "basecamp/html_email-noIdentity-stripe.html",
+                {'name': instance.name, 'email': instance.email, 'amount': instance.amount}
+            )
+            payment_send_email("Payment - EasyGo", html_content, [instance.email, RECIPIENT_EMAIL])
