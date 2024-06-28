aed0ae5cef65f60f2d25b6dc210f3effbcfe25e3
sungkam718 <sungkam718@gmail.com>
2024-06-28 19:43:05 +1000

    send_apology_emails: can be tried 7 times in 1 min

--- blog/tasks.py
+++ blog/tasks.py
@@ -143,20 +143,26 @@ def send_email_task(pickup_date, direction, suburb, no_of_passenger):
 
 
 # Review page, suburbs page, service, information, about_us, terms, policy, suburbs1
+RATE_LIMIT_KEY_TEMPLATE = 'rate_limit_{email}'
+RATE_LIMIT_COUNT_KEY_TEMPLATE = 'rate_limit_count_{email}'
+RATE_LIMIT_MAX_ATTEMPTS = 7  # 1분 안에 허용되는 최대 이메일 전송 횟수
+
 @shared_task(bind=True, max_retries=3)
 def send_notice_email(self, subject, message, RECIPIENT_EMAIL):
     try:
         if not all([subject, message, RECIPIENT_EMAIL]):
             raise ValueError("Subject, message, and recipient email must be provided")
-
-        # Ensure DEFAULT_FROM_EMAIL is set in environment variables
-        DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+        
         if not DEFAULT_FROM_EMAIL:
             raise ImproperlyConfigured("DEFAULT_FROM_EMAIL is not set in environment variables")
 
         # Check rate limit
         rate_limit_key = RATE_LIMIT_KEY_TEMPLATE.format(email=RECIPIENT_EMAIL)
-        if cache.get(rate_limit_key):
+        rate_limit_count_key = RATE_LIMIT_COUNT_KEY_TEMPLATE.format(email=RECIPIENT_EMAIL)
+        
+        rate_limit_count = cache.get(rate_limit_count_key, 0)
+
+        if rate_limit_count >= RATE_LIMIT_MAX_ATTEMPTS:
             logger.warning(f"Rate limit exceeded for {RECIPIENT_EMAIL}")
             return
 
@@ -169,14 +175,15 @@ def send_notice_email(self, subject, message, RECIPIENT_EMAIL):
         )
         logger.info(f"Email sent to {RECIPIENT_EMAIL} with subject {subject}")
 
-        # Set rate limit key with a timeout (e.g., 60 seconds)
+        # Increment rate limit count and set rate limit key with a timeout (e.g., 60 seconds)
         cache.set(rate_limit_key, 'sent', timeout=60)
+        cache.set(rate_limit_count_key, rate_limit_count + 1, timeout=60)
 
     except (ValueError, ImproperlyConfigured) as e:
         logger.error(f"Failed to send email: {str(e)}")
     except Exception as e:
         logger.error(f"An unexpected error occurred: {str(e)}")
-        self.retry(countdown=60)  # Retry after 60 seconds if there is an unexpected error
+        self.retry(countdown=60)
     
 
 def payment_send_email(subject, html_content, recipient_list):
