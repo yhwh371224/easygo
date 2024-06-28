28a5218392ce823d5a65219adc5f6293ee492ce7
sungkam718 <sungkam718@gmail.com>
2024-06-28 12:21:57 +1000

    4/11 - 5/11 for sending apology emails

--- blog/management/commands/send_apology_emails.py
+++ blog/management/commands/send_apology_emails.py
@@ -10,8 +10,8 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         today = timezone.now().date()
-        start_date = today - timedelta(days=(today.day - 12))  # Calculate June 12th
-        end_date = start_date - timedelta(days=30)  # Calculate May 11th
+        start_date = today.replace(month=5, day=11)
+        end_date = today.replace(month=4, day=11)
 
         customers = Post.objects.filter(created__range=[end_date, start_date])
 
