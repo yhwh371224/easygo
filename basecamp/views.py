cccb9b3176a0898dc8d66ed4e308fc647c2b5a3d
sungkam718 <sungkam718@gmail.com>
2024-07-04 15:41:22 +1000

    invoice cash fixed

--- basecamp/views.py
+++ basecamp/views.py
@@ -1579,15 +1579,14 @@ def invoice_detail(request):
 
         if user.cash and user.paid:
             html_content = render_to_string("basecamp/html_email-invoice-cash.html",
-                                        {'inv_no': inv_no, 'name': user.name, 'company_name': user1.company_name, 'contact': user1.contact, 'discount': discount,
-                                        'email': user1.email, 'direction': user1.direction, 'pickup_date': user1.pickup_date, 'invoice_date': today, 
-                                        'flight_number': user1.flight_number, 'flight_time': user1.flight_time, 'pickup_time': user1.pickup_time,
-                                        'return_direction': user1.return_direction, 'return_pickup_date': user1.return_pickup_date,
-                                        'return_flight_number': user1.return_flight_number, 'return_flight_time': user1.return_flight_time, 'return_pickup_time': user1.return_pickup_time,
-                                        'street': user1.street, 'suburb': user1.suburb, 'no_of_passenger': user1.no_of_passenger, 'no_of_baggage': user1.no_of_baggage,
-                                        'price': user1.price, 'with_gst': with_gst, 'surcharge': surcharge, 'total_price': total_price, 'toll': toll, 
-                                        'balance': balance, 'paid': float_paid, 'message': user1.message })
-
+                                        {'inv_no': inv_no, 'name': user.name, 'company_name': user.company_name,'contact': user.contact, 'discount': discount,
+                                        'email': email, 'direction': user.direction, 'pickup_date': user.pickup_date, 'invoice_date': today,
+                                        'flight_number': user.flight_number, 'flight_time': user.flight_time, 'pickup_time': user.pickup_time,
+                                        'return_direction': user.return_direction, 'return_pickup_date': user.return_pickup_date,
+                                        'return_flight_number': user.return_flight_number, 'return_flight_time': user.return_flight_time, 'return_pickup_time': user.return_pickup_time,
+                                        'street': user.street, 'suburb': user.suburb, 'no_of_passenger': user.no_of_passenger, 'no_of_baggage': user.no_of_baggage,
+                                        'price': user.price, 'with_gst': with_gst, 'surcharge': surcharge, 'total_price': total_price, 'toll': toll, 
+                                        'balance': balance, 'paid': float_paid, 'message': user.message })
             text_content = strip_tags(html_content)
 
             recipient_list = [email, RECIPIENT_EMAIL]
