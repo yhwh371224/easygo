b71867aa8fa0f2f2a313e4807f04adf7203a8405
sungkam718 <sungkam718@gmail.com>
2024-07-09 14:16:07 +1000

    gmail_api updated with good news for awards

--- blog/management/commands/gmail_api.py
+++ blog/management/commands/gmail_api.py
@@ -10,16 +10,17 @@ import base64
 
 
 class Command(BaseCommand):
-    help = 'Send apology emails to customers created in the last three months'
+    help = 'Send good news to customers'
 
     def handle(self, *args, **kwargs):
         today = timezone.now().date()
         start_date = timezone.datetime(2023, 2, 9).date()
-        end_date = timezone.datetime(2023, 4, 5).date()
+        end_date = timezone.datetime(2023, 7, 7).date()
 
         # Filter customers where Pickup_date is missing
         customers = Post.objects.filter(
             created__range=[start_date, end_date],
+            cancelled=True, 
             # Pickup_date__isnull=True
         )
 
@@ -33,20 +34,17 @@ class Command(BaseCommand):
             # Call the Gmail API
             service = build('gmail', 'v1', credentials=delegated_creds)
 
-            subject = 'Apology for Recent Email Error'
+            subject = 'Exciting News from EasyGo Airport Shuttle!'
             message_body = (
                 "Dear Valued Customer,\n\n"
-                "I hope this email finds you well.\n\n"
-                "We sincerely apologize for any inconvenience caused by a recent system issue that resulted in data loss, specifically booking dates (pickup dates). "
-                "We have diligently worked to manually recover all the lost information and I am pleased to confirm that we have restored all booking details\n\n"
-                "However, to ensure no errors occur, we kindly request your assistance."
-                "If you do not receive a reminder email three days prior to your booking date, please contact us immediately to confirm your booking details. \n\n"                
-                "We deeply regret any inconvenience this may cause and appreciate your understanding"
-                "We are taking these steps to ensure no mistakes are made and to provide you with the best service possible.\n\n"
-                "Thank you for your understanding and patience.\n\n"
+                "We are excited to share some wonderful news with you! EasyGo Airport Shuttle has been awarded the 2024 Quality Business Award by the Inner West Council. "
+                "This recognition is a testament to our commitment to providing excellent service to all our customers.\n\n"
+                "We want to express our sincere gratitude for your continued support, which has been instrumental in achieving this honor.\n\n"
+                "For more details about this award and what it means for EasyGo Airport Shuttle, please click the link below:\n"
+                "https://qualitybusinessawards.com.au/2024/the-best-airport-shuttle-service-in-inner-west-council/easygo-airport-shuttle\n\n"
+                "Thank you for choosing EasyGo Airport Shuttle. We look forward to serving you again soon.\n\n"
                 "Best regards,\n\n"
-                "Peter\n"
-                "EasyGo Airport Shuttle\n"
+                "The EasyGo Airport Shuttle Team\n"
             )
 
             sent_emails = set()  
