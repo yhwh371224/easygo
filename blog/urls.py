78b6efe16cfaadae6b827a1ea4125c2cfdc524df
sungkam718 <sungkam718@gmail.com>
2024-07-12 19:54:49 +1000

    blog.urls and views. and .gitignore

--- blog/urls.py
+++ blog/urls.py
@@ -3,11 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('search/<str:q>/', views.PostSearch.as_view()),
-    path('<int:pk>/update/', views.PostUpdate.as_view(), name="blog/<pk>/update/"),
-    path('<int:pk>/', views.PostDetail.as_view(), name="blog/<pk>"),
-    path('create/', views.PostCreate.as_view(), name="blog/create"),
-    path('', views.PostList.as_view(), name="blog"),
 
 ]
 
