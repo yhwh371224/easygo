ebd4d4e7a85cc5f3805ae6d30fecfc48e4c6921f
sungkam718 <sungkam718@gmail.com>
2024-07-21 20:42:44 +1000

    removed itsdangerous

--- easygo_review/urls.py
+++ easygo_review/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path('custom_login/', views.custom_login_view, name='custom_login'),
     path('custom_logout/', views.custom_logout_view, name='custom_logout'),
     path('recaptcha-verify/', views.recaptcha_verify, name='recaptcha_verify'),
-    path('verify-email/<uidb64>/', views.verify_email, name='verify_email'),
     path('search/<str:q>/', views.PostSearch.as_view(), name="post_search"),
     path('<int:pk>/', views.PostDetail.as_view(), name="easygo_review/<pk>"),
     path('<int:pk>/update/', views.PostUpdate.as_view(), name="easygo_review/<pk>/update/"),
