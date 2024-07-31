3df631118354d5df5fc8e2cb323ed7b159fae10d
sungkam718 <sungkam718@gmail.com>
2024-08-01 06:11:30 +1000

    settings.py and context_processors.py for admin honey pot error

--- context_processors.py
+++ context_processors.py
@@ -6,3 +6,12 @@ def recaptcha_site_key(request):
         'RECAPTCHA_V2_SITE_KEY': settings.RECAPTCHA_V2_SITE_KEY,
         'RECAPTCHA_V3_SITE_KEY': settings.RECAPTCHA_V3_SITE_KEY,
     }
+
+def add_custom_context(request):
+    return {
+        'is_nav_sidebar_enabled': False,
+        'is_popup': False,
+        'site_header': 'EasyGo Administration',
+        'site_title': 'EasyGo Admin',   
+        'subtitle': 'Welcome to the admin panel',  
+    }
