1d7b748532f59e456a2debf056e1c813aefe7f28
sungkam718 <sungkam718@gmail.com>
2024-08-03 00:33:16 +1000

    comment removed

--- easygo_review/urls.py
+++ easygo_review/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('search/<str:q>/', views.PostSearch.as_view(), name="post_search"),
     path('<int:pk>/', views.PostDetail.as_view(), name="easygo_review/<pk>"),
     path('<int:pk>/update/', views.PostUpdate.as_view(), name="easygo_review/<pk>/update/"),
-    path('<int:pk>/new_comment/', views.new_comment, name="easygo_review/<pk>/new_comment/"),
-    path('edit_comment/<int:pk>/', views.CommentUpdate.as_view(), name='edit_comment'),
-    path('delete_comment/<int:pk>/', views.delete_comment, name='delete_comment'),
+    # path('<int:pk>/new_comment/', views.new_comment, name="easygo_review/<pk>/new_comment/"),
+    # path('edit_comment/<int:pk>/', views.CommentUpdate.as_view(), name='edit_comment'),
+    # path('delete_comment/<int:pk>/', views.delete_comment, name='delete_comment'),
 ]
