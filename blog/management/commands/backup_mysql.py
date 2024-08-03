2d5dc5deafcadb69828f39b891bfbb356f01006d
yhwh371224 <sungkam3@gmail.com>
2024-08-04 05:26:38 +1000

    commands backup_mysql.py updated and requirements

--- blog/management/commands/backup_mysql.py
+++ blog/management/commands/backup_mysql.py
@@ -1,67 +1,72 @@
+from django.core.management.base import BaseCommand
 import sqlite3
 import mysql.connector
 
-sqlite_conn = sqlite3.connect('/home/ubuntu/github/easygo/db.sqlite3')
-sqlite_cursor = sqlite_conn.cursor()
+class Command(BaseCommand):
+    help = 'Backup data from SQLite to MySQL'
 
-mysql_conn = mysql.connector.connect(
-    host='localhost',
-    user='mysql-easygo',
-    password='inri.J1919',
-    database='easygobank'
-)
-mysql_cursor = mysql_conn.cursor()
+    def handle(self, *args, **options):
+        sqlite_conn = sqlite3.connect('/home/ubuntu/github/easygo/db.sqlite3')
+        sqlite_cursor = sqlite_conn.cursor()
 
-sqlite_cursor.execute('SELECT * FROM blog_post')
-rows = sqlite_cursor.fetchall()
+        mysql_conn = mysql.connector.connect(
+            host='localhost',
+            user='mysql-easygo',
+            password='inri.J1919',
+            database='easygobank'
+        )
+        mysql_cursor = mysql_conn.cursor()
 
-insert_query = '''
-INSERT INTO blog_post (
-    name,
-    company_name,
-    contact,
-    email,
-    email1,
-    pickup_date,
-    flight_number,
-    flight_time,
-    pickup_time,
-    direction,
-    suburb,
-    street,
-    no_of_passenger,
-    no_of_baggage,
-    return_direction,
-    return_pickup_date,
-    return_flight_number,
-    return_flight_time,
-    return_pickup_time,
-    message,
-    notice,
-    price,
-    paid,
-    discount,
-    toll,
-    driver,
-    meeting_point,
-    is_confirmed,
-    cash,
-    cruise,
-    cancelled,
-    private_ride,
-    reminder,
-    sent_email,
-    calendar_event_id
-) VALUES (
-    %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
-)'''
+        sqlite_cursor.execute('SELECT * FROM blog_post')
+        rows = sqlite_cursor.fetchall()
 
-for row in rows:
-    mysql_cursor.execute(insert_query, row[1:])  
+        insert_query = '''
+        INSERT INTO blog_post (
+            name,
+            company_name,
+            contact,
+            email,
+            email1,
+            pickup_date,
+            flight_number,
+            flight_time,
+            pickup_time,
+            direction,
+            suburb,
+            street,
+            no_of_passenger,
+            no_of_baggage,
+            return_direction,
+            return_pickup_date,
+            return_flight_number,
+            return_flight_time,
+            return_pickup_time,
+            message,
+            notice,
+            price,
+            paid,
+            discount,
+            toll,
+            driver,
+            meeting_point,
+            is_confirmed,
+            cash,
+            cruise,
+            cancelled,
+            private_ride,
+            reminder,
+            sent_email,
+            calendar_event_id
+        ) VALUES (
+            %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
+        )'''
 
-mysql_conn.commit()
+        for row in rows:
+            mysql_cursor.execute(insert_query, row[1:])  
 
-sqlite_conn.close()
-mysql_conn.close()
+        mysql_conn.commit()
 
-print("Data migration from SQLite to MySQL completed successfully.")
+        sqlite_conn.close()
+        mysql_conn.close()
+
+        self.stdout.write(self.style.SUCCESS('Data migration from SQLite to MySQL completed successfully.'))
