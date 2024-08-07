f4628c0b9e813ac506795cb0580ae58e5b9a9d05
sungkam718 <sungkam718@gmail.com>
2024-08-07 20:32:34 +1000

    twilio upgraded

--- blog/management/commands/send_sms.py
+++ blog/management/commands/send_sms.py
@@ -8,7 +8,7 @@ from decouple import config
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 class Command(BaseCommand):
-    help = 'Send final notices'
+    help = 'Send final notices via WhatsApp or SMS'
 
     def handle(self, *args, **options):
         tomorrow = date.today() + timedelta(days=1)
@@ -19,26 +19,59 @@ class Command(BaseCommand):
         auth_token = config('TWILIO_AUTH_TOKEN')
         client = Client(account_sid, auth_token)
 
+        # List of office numbers
+        office_numbers = [
+            '+61406783559',  
+        ]
+
         def format_phone_number(phone_number):
+            # Ensure the phone number is in E.164 format
             if not phone_number.startswith('+'):
                 phone_number = '+61' + phone_number.lstrip('0')
             return phone_number
 
-        def sms_one(sendto):
+        def send_whatsapp_message(sendto):
             formatted_number = format_phone_number(sendto)
-            client.messages.create(
+            message = client.messages.create(
                 body="EasyGo - Urgent notice \
                       \n\nWe haven't received your payment and a response to our emails. \
                       \nPlease contact us ASAP or your booking may be canceled. \
                       \nReply only via email >> info@easygoshuttle.com.au",
-                from_='+18148920523',
+                from_='whatsapp:+14155238886',  # Your Twilio WhatsApp-enabled number
+                to=f'whatsapp:{formatted_number}'
+            )
+            return message.sid
+
+        def send_sms_message(sendto):
+            formatted_number = format_phone_number(sendto)
+            message = client.messages.create(
+                body="EasyGo - Urgent notice \
+                      \n\nWe haven't received your payment and a response to our emails. \
+                      \nPlease contact us ASAP or your booking may be canceled. \
+                      \nReply only via email >> info@easygoshuttle.com.au",
+                from_='+18148920523',  # Your Twilio SMS number
                 to=formatted_number
             )
+            return message.sid
 
+        sent_numbers = []  # List to keep track of sent numbers
         for final_notice in final_notices:
             if not final_notice.reminder and not final_notice.cancelled and not final_notice.paid:
-                sms_contact = final_notice.contact  
-                if sms_contact:
-                    sms_one(sms_contact)
+                if final_notice.direction == 'Pickup from Intl Airport':
+                    message_sid = send_whatsapp_message(final_notice.contact)
+                else:
+                    message_sid = send_sms_message(final_notice.contact)
+                
+                if final_notice.contact:
+                    sent_numbers.append((final_notice.contact, message_sid))
 
-        self.stdout.write(self.style.SUCCESS('Successfully sent final notices via SMS'))
+        # Send to office numbers
+        for office_number in office_numbers:
+            message_sid = send_sms_message(office_number)
+            sent_numbers.append((office_number, message_sid))
+
+        # Log the sent numbers
+        for number, sid in sent_numbers:
+            self.stdout.write(self.style.SUCCESS(f'Message sent to {number}, Message SID: {sid}'))
+
+        self.stdout.write(self.style.SUCCESS('Successfully sent final notices'))
