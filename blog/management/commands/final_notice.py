a53377fc2c76ba1666914b4db2e55c630a7cc061
sungkam718 <sungkam718@gmail.com>
2024-08-09 09:28:44 +1000

    final_notice today ftoday.html added

--- blog/management/commands/final_notice.py
+++ blog/management/commands/final_notice.py
@@ -19,16 +19,18 @@ class Command(BaseCommand):
         final_notices = Post.objects.filter(pickup_date__range=[today, tomorrow])
         
         for final_notice in final_notices:
-
             if not final_notice.reminder and not final_notice.cancelled and not final_notice.paid:
-                 
-                html_content = render_to_string("basecamp/html_email-fnotice.html",
-                                                {'name': final_notice.name, 'email': final_notice.email})
+                if final_notice.pickup_date == today:
+                    template_name = "basecamp/ftoday.html"
+                else:
+                    template_name = "basecamp/html_email-fnotice.html"
+                
+                html_content = render_to_string(template_name, {'name': final_notice.name, 'email': final_notice.email})
                 text_content = strip_tags(html_content)
                 email = EmailMultiAlternatives("Final notice", text_content, '', [final_notice.email, RECIPIENT_EMAIL])
                 email.attach_alternative(html_content, "text/html")
                 email.send()
-            
+
 
             
 
