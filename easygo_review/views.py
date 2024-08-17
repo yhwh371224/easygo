d649c9638c97527e019f695b154cf85784e177a3
sungkam718 <sungkam718@gmail.com>
2024-08-17 15:35:20 +1000

    new comment 2

--- easygo_review/views.py
+++ easygo_review/views.py
@@ -178,28 +178,17 @@ class PostUpdate(UpdateView):
     
 
 def new_comment(request, pk):
-    blog_post = BlogPost.objects.get(pk=pk)
+    post = Post.objects.get(pk=pk)
 
     if request.method == 'POST':
-        email = request.session.get('email')
-        if not email:
-            return redirect('login')  
-
         comment_form = CommentForm(request.POST)
         if comment_form.is_valid():
             comment = comment_form.save(commit=False)
-            comment.post = blog_post
-            comment.email = email  
-
-            post = Post.objects.filter(email=email).first()
-            if post:
-                comment.author = post.name  
-            else:
-                raise PermissionDenied('error!')
-            comment.save()  
-            return redirect(comment.get_absolute_url())  
-        
-    return redirect('/easygo_review/')   
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            return redirect(comment.get_absolute_url())
+    return redirect('/easygo_review/')    
 
 
 class CommentUpdate(UpdateView):
