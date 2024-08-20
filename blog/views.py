95928e374378f578efcb3906ddd10b200386bf5a
sungkam718 <sungkam718@gmail.com>
2024-08-21 07:57:23 +1000

    test-500 added

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,31 @@
-from django.shortcuts import redirect
-from django.db.models import Q
+from django.shortcuts import render
+from django.http import HttpResponseServerError
 
 
+# error handler 400 403 404 500 502 503 
+def custom_bad_request(request, exception):
+    return render(request, '400.html', status=400)
 
+
+def custom_forbidden(request, exception):
+    return render(request, '403.html', status=403)
+
+
+def custom_page_not_found(request, exception):
+    return render(request, '404.html', status=404)
+
+
+def custom_server_error(request, exception):
+    return render(request, '500.html', status=500)
+
+
+def custom_bad_gateway(request, exception):
+    return render(request, '502.html', status=502)
+
+
+def custom_under_maintenance(request, exception):
+    return render(request, '503.html', status=503)
+
+
+def test_server_error(request):
+    return HttpResponseServerError("This is a test 500 error.")
