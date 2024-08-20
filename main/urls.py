e9c2bc87aa9060f822ffc769d1f2199eab932ea7
sungkam718 <sungkam718@gmail.com>
2024-08-21 07:57:23 +1000

    test-500 added

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 from django.views.static import serve
 from django.contrib.auth.decorators import user_passes_test
 from basecamp.views import stripe_webhook
+from blog.views import test_server_error
 
 
 admin_site = user_passes_test(lambda u: u.is_superuser)(admin.site.urls)
@@ -19,6 +20,7 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('paypal/', include('paypal.standard.ipn.urls')),
     path('stripe_webhook/', stripe_webhook, name='stripe_webhook'),
+    path('test-500/', test_server_error, name='test_server_error'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
@@ -28,9 +30,9 @@ admin.site.site_title = "EasyGo Administration"
 admin.site.index_title = "EasyGo admin"
 admin.site.block_title = "EasyGo Admin"
 
-handler400 = 'basecamp.views.custom_bad_request'
-handler403 = 'basecamp.views.custom_forbidden'
-handler404 = 'basecamp.views.custom_page_not_found'
-handler500 = 'basecamp.views.custom_server_error'
-handler502 = 'basecamp.views.custom_bad_gateway'
-handler503 = 'basecamp.views.custom_under_maintenance'
+handler400 = 'blog.views.custom_bad_request'
+handler403 = 'blog.views.custom_forbidden'
+handler404 = 'blog.views.custom_page_not_found'
+handler500 = 'blog.views.custom_server_error'
+handler502 = 'blog.views.custom_bad_gateway'
+handler503 = 'blog.views.custom_under_maintenance'
