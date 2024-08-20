22cd8e492bc81240d3d9ba532434ca1855575804
sungkam718 <sungkam718@gmail.com>
2024-08-21 09:36:26 +1000

    final_notice and no_payment_yet self.stdout.write added

--- blog/management/commands/final_notice.py
+++ blog/management/commands/final_notice.py
@@ -14,22 +14,28 @@ class Command(BaseCommand):
     help = 'Send final notices'
 
     def handle(self, *args, **options):
-        today = date.today()
-        tomorrow = today + timedelta(days=1)
-        final_notices = Post.objects.filter(pickup_date__range=[today, tomorrow])
-        
-        for final_notice in final_notices:
-            if not final_notice.reminder and not final_notice.cancelled and not final_notice.paid:
-                if final_notice.pickup_date == today:
-                    template_name = "basecamp/ftoday.html"
-                else:
-                    template_name = "basecamp/html_email-fnotice.html"
-                
-                html_content = render_to_string(template_name, {'name': final_notice.name, 'email': final_notice.email})
-                text_content = strip_tags(html_content)
-                email = EmailMultiAlternatives("Final notice", text_content, '', [final_notice.email, RECIPIENT_EMAIL])
-                email.attach_alternative(html_content, "text/html")
-                email.send()
+        try: 
+            today = date.today()
+            tomorrow = today + timedelta(days=1)
+            final_notices = Post.objects.filter(pickup_date__range=[today, tomorrow])
+            
+            for final_notice in final_notices:
+                if not final_notice.reminder and not final_notice.cancelled and not final_notice.paid:
+                    if final_notice.pickup_date == today:
+                        template_name = "basecamp/ftoday.html"
+                    else:
+                        template_name = "basecamp/html_email-fnotice.html"
+                    
+                    html_content = render_to_string(template_name, {'name': final_notice.name, 'email': final_notice.email})
+                    text_content = strip_tags(html_content)
+                    email = EmailMultiAlternatives("Final notice", text_content, '', [final_notice.email, RECIPIENT_EMAIL])
+                    email.attach_alternative(html_content, "text/html")
+                    email.send()
+
+            self.stdout.write(self.style.SUCCESS('Email sent final notice successfully'))
+
+        except Exception as e:            
+            self.stdout.write(self.style.ERROR('Failed to send final notices via email'))
 
 
             
