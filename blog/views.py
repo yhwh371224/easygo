66c62a6f61c9bed954c97e3667aa4a721b3998d6
sungkam718 <sungkam718@gmail.com>
2024-08-21 09:22:21 +1000

    error handler request argument only

--- blog/views.py
+++ blog/views.py
@@ -3,27 +3,27 @@ from django.http import HttpResponseServerError
 
 
 # error handler 400 403 404 500 502 503 
-def custom_bad_request(request, exception):
+def custom_bad_request(request):
     return render(request, '400.html', status=400)
 
 
-def custom_forbidden(request, exception):
+def custom_forbidden(request):
     return render(request, '403.html', status=403)
 
 
-def custom_page_not_found(request, exception):
+def custom_page_not_found(request):
     return render(request, '404.html', status=404)
 
 
-def custom_server_error(request, exception):
+def custom_server_error(request):
     return render(request, '500.html', status=500)
 
 
-def custom_bad_gateway(request, exception):
+def custom_bad_gateway(request):
     return render(request, '502.html', status=502)
 
 
-def custom_under_maintenance(request, exception):
+def custom_under_maintenance(request):
     return render(request, '503.html', status=503)
 
 
