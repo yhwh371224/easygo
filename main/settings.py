67ae5e53738cea244b072a7b80ebb98239aec2b6
sungkam718 <sungkam718@gmail.com>
2024-08-21 19:41:41 +1000

    fix_requirements.py, ... LF will be replaced by CRLF

--- main/settings.py
+++ main/settings.py
@@ -267,7 +267,7 @@ HTML_MINIFY = True
 KEEP_COMMENTS_ON_MINIFYING = True
 
 LOGIN_REDIRECT_URL = '/easygo_review/'
-LOGOUT_REDIRECT_URL = '/home/'
+LOGOUT_REDIRECT_URL = '/easygo_review/'
 
 CORS_ALLOWED_ORIGINS = [
     "https://easygoshuttle.com.au",
