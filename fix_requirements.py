89a58f643dab585ffd3795e9c437b0f058ced4c5
yhwh371224 <sungkam3@gmail.com>
2024-08-23 09:08:07 +1000

    requirements.txt and fix_requirements.py updated

--- fix_requirements.py
+++ fix_requirements.py
@@ -1,6 +1,6 @@
-# import re
+import re
 
-# # 읽기/쓰기 모드로 파일 열기
+# # 공백이 있는 경우: 공백제거하고 >= 연결하기
 # with open('requirements.in', 'r', encoding='utf-8') as file:
 #     lines = file.readlines()
 
@@ -20,45 +20,51 @@
 
 # print("File has been fixed successfully.")
 
-# import re
 
-# # 읽기/쓰기 모드로 파일 열기
-# with open('requirements.in', 'r', encoding='utf-8') as file:
-#     lines = file.readlines()
+# # 공백이 없는 경우: >= 로 바꾸기 
+with open('requirements.in', 'r', encoding='utf-8') as file:
+    lines = file.readlines()
 
-# # ==를 >=로 바꾸기
-# fixed_lines = []
-# for line in lines:
-#     fixed_line = re.sub(r'==', '>=', line)
-#     fixed_lines.append(fixed_line)
+fixed_lines = []
+for line in lines:
+    fixed_line = re.sub(r'==', '>=', line)
+    fixed_lines.append(fixed_line)
 
-# # 수정된 내용을 파일에 다시 쓰기
-# with open('requirements.in', 'w', encoding='utf-8') as file:
-#     file.writelines(fixed_lines)
+with open('requirements.in', 'w', encoding='utf-8') as file:
+    file.writelines(fixed_lines)
 
-# print("File has been fixed successfully.")
+print("File has been fixed successfully.")
+
+# # requirements.txt 파일에 주석된 부분만 모두 제거하기
+# def remove_comments_from_requirements(file_path):
+#     try:
+#         # 파일 읽기
+#         with open(file_path, 'r', encoding='utf-8') as file:
+#             lines = file.readlines()
+        
+#         print("File read successfully.")
+        
+#         # 주석을 제거하고 주석이 없는 줄만 유지
+#         cleaned_lines = []
+#         for line in lines:
+#             # 주석 제거
+#             cleaned_line = re.sub(r'#.*', '', line).rstrip()
+#             if cleaned_line:  # 비어있지 않은 줄만 추가
+#                 cleaned_lines.append(line)  # 원래의 줄을 그대로 유지
+        
+#         print("Comments removed. Lines prepared for writing.")
+        
+#         # 수정된 내용을 파일에 다시 쓰기
+#         with open(file_path, 'w', encoding='utf-8') as file:
+#             file.writelines(cleaned_lines)
+        
+#         print("Comments have been removed successfully.")
+    
+#     except Exception as e:
+#         print(f"An error occurred: {e}")
+
+# # 예시로 파일 경로를 입력하여 함수 호출
+# remove_comments_from_requirements('requirements.txt')
 
 
-# 주석을 제거하는 스크립트
-
-import re
-
-def remove_comments_from_requirements(file_path):
-    # 파일 읽기
-    with open(file_path, 'r', encoding='utf-8') as file:
-        lines = file.readlines()
-
-    # 주석을 제거하고 주석이 아닌 줄만 유지
-    cleaned_lines = []
-    for line in lines:
-        # 주석이 시작되는 '# ' 또는 '#' 뒤의 공백을 제거
-        cleaned_line = re.sub(r'#.*', '', line).strip()
-        if cleaned_line:  # 비어있지 않은 줄만 추가
-            cleaned_lines.append(cleaned_line + '\n')
-
-    # 수정된 내용을 파일에 다시 쓰기
-    with open(file_path, 'w', encoding='utf-8') as file:
-        file.writelines(cleaned_lines)
-
-    print("Comments have been removed successfully.")
 
