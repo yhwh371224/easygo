7dd1f76d8bc16c340a540f7e8e6ac05cfa5fc57f
sungkam718 <sungkam718@gmail.com>
2024-09-23 20:43:55 +1000

    added block ip

--- main/middlewares/block_ip_middleware.py
+++ main/middlewares/block_ip_middleware.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponseForbidden
 
 class BlockIPMiddleware:
-    BLOCKED_IPS = ['207.154.205.99', '147.78.47.81', '27.210.152.236', '157.66.54.6']
+    BLOCKED_IPS = ['207.154.205.99', '147.78.47.81', '27.210.152.236', '157.66.54.6', '43.128.84.144']
 
     def __init__(self, get_response):
         self.get_response = get_response
