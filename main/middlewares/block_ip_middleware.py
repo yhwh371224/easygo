5580c5f232417e39336fec3f9dd583b718537349
sungkam718 <sungkam718@gmail.com>
2024-10-14 16:13:52 +1100

    new ip codes updated

--- main/middlewares/block_ip_middleware.py
+++ main/middlewares/block_ip_middleware.py
@@ -7,9 +7,17 @@ class BlockIPMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        ip = request.META.get('REMOTE_ADDR')
+        ip = self.get_client_ip(request)
         if ip in self.BLOCKED_IPS:
             return HttpResponseForbidden("Forbidden: Your IP address is blocked.")
         
         response = self.get_response(request)
         return response
+
+    def get_client_ip(self, request):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0].strip()
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
