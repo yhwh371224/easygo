7616053d72f188ee7add4ef14d0e7ac3ed4f39da
sungkam718 <sungkam718@gmail.com>
2024-11-20 09:53:44 +1100

    gratitude payment 0.00

--- basecamp/views.py
+++ basecamp/views.py
@@ -1721,12 +1721,12 @@ def email_dispatch_detail(request):
                     })
 
             if selected_option == "Gratitude For Payment" and user:                     
-                user.paid = float(user.price) + 0.10
+                user.paid = float(user.price) + 0.00
                 user.reminder = True
                 user.save()
                 if user.return_pickup_time == 'x':
                     user_1 = Post.objects.filter(email=email)[1]
-                    user_1.paid = float(user.price) + 0.10
+                    user_1.paid = float(user.price) + 0.00
                     user_1.reminder = True
                     user_1.save() 
             
