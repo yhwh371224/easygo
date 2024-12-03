3d2a61c10e2941128e16197f89b5edb676ee407e
sungkam718 <sungkam718@gmail.com>
2024-12-03 11:12:58 +1100

    return_start_point and return_end_point added for return booking

--- blog/signals.py
+++ blog/signals.py
@@ -80,10 +80,10 @@ def notify_user_post(sender, instance, created, **kwargs):
     elif not instance.calendar_event_id and instance.return_pickup_time:
         p = Post(name=instance.name, contact=instance.contact, email=instance.email, company_name=instance.company_name, email1=instance.email1, 
                  pickup_date=instance.return_pickup_date, flight_number=instance.return_flight_number, flight_time=instance.return_flight_time, 
-                 pickup_time=instance.return_pickup_time, direction=instance.return_direction, suburb=instance.suburb, street=instance.street, 
-                 no_of_passenger=instance.no_of_passenger, no_of_baggage=instance.no_of_baggage, message=instance.message, return_pickup_time="x",
-                 return_pickup_date=instance.pickup_date, notice=instance.notice, price=instance.price, paid=instance.paid, private_ride=instance.private_ride, 
-                 driver=instance.driver,)
+                 pickup_time=instance.return_pickup_time, direction=instance.return_direction, start_point=instance.return_start_point, 
+                 end_point=instance.return_end_point, suburb=instance.suburb, street=instance.street, no_of_passenger=instance.no_of_passenger, 
+                 no_of_baggage=instance.no_of_baggage, message=instance.message, return_pickup_time="x", return_pickup_date=instance.pickup_date, 
+                 notice=instance.notice, price=instance.price, paid=instance.paid, private_ride=instance.private_ride, driver=instance.driver,)
 
         p.save() 
 
