3ed1b31b0479c190413cbd454b73747f1fb57cb7
sungkam718 <sungkam718@gmail.com>
2025-06-04 07:20:06 +1000

    updated 36: reminder, html late, early ...

--- blog/management/commands/delete_cancelled.py
+++ blog/management/commands/delete_cancelled.py
@@ -1,17 +1,16 @@
 from django.core.management.base import BaseCommand
-from django.apps import apps
-from django.db import models
+from blog.models import Post, Inquiry
 
 
 class Command(BaseCommand):
-    help = "Delete records with cancelled=True from all blog models that have a cancelled field."
+    help = "Delete records with cancelled=True from Post and Inquiry models only."
 
     def handle(self, *args, **options):
-        blog_models = apps.get_app_config('blog').get_models()
+        models_to_check = [Post, Inquiry]
         total_deleted = 0
 
-        for model in blog_models:
-            if 'cancelled' in [field.name for field in model._meta.get_fields()]:
+        for model in models_to_check:
+            if 'cancelled' in [field.name for field in model._meta.fields]:
                 deleted, _ = model.objects.filter(cancelled=True).delete()
                 if deleted > 0:
                     self.stdout.write(f"{model.__name__}: Deleted {deleted} cancelled records.")
@@ -22,5 +21,6 @@ class Command(BaseCommand):
                 self.stdout.write(f"{model.__name__}: No 'cancelled' field. Skipping.")
 
         self.stdout.write(self.style.SUCCESS(f"Finished. Total deleted: {total_deleted} records."))
+
 
 
