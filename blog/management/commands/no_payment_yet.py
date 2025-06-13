3f8f82d45619a585b15a643ab02e86f9469ebaa5
sungkam718 <sungkam718@gmail.com>
2025-06-13 12:45:53 +1000

    updated 74 cancelled exclude

--- blog/management/commands/no_payment_yet.py
+++ blog/management/commands/no_payment_yet.py
@@ -25,7 +25,7 @@ class Command(BaseCommand):
             bookings = Post.objects.filter(pickup_date__range=(start_date, end_date))
                 
             for booking in bookings:
-                if not booking.cash and not booking.paid and not booking.cancelled:
+                if not booking.cash and (booking.paid is None or booking.paid.strip() in ["", "0"]) and not booking.cancelled:
                     days_difference = (booking.pickup_date - start_date).days
                     if days_difference in [0, 1, 2]:  
                         email_subject = "Urgent notice for payment"
