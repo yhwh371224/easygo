fdca8bd68edf6186dcfc7eda266ef84fc9641dfc
yhwh371224 <sungkam3@gmail.com>
2025-06-27 07:38:02 +1000

    update from laptop

--- blog/signals.py
+++ blog/signals.py
@@ -1,11 +1,14 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.template.loader import render_to_string
+from utils.email_helper import EmailSender
+from main.settings import RECIPIENT_EMAIL
 
 from .models import Post, Inquiry, PaypalPayment, StripePayment
 from .tasks import create_event_on_calendar, notify_user_payment_paypal, notify_user_payment_stripe
 from utils.return_booking import handle_return_trip
 from utils.inquiry_helper import send_inquiry_email  
-from utils.post_helper import send_post_cancelled_email
+from utils.post_helper import send_post_cancelled_email, send_missing_direction_email
 
 
 @receiver(post_save, sender=Inquiry, dispatch_uid="notify_user_inquiry_once")
@@ -43,5 +46,11 @@ def async_notify_user_payment_stripe(sender, instance, created, **kwargs):
 def async_create_event_on_calendar(sender, instance, created, **kwargs):
     create_event_on_calendar.delay(instance.id)
 
+
+# check missing direction when flight number exists
+@receiver(post_save, sender=Post, dispatch_uid="check_missing_direction_once")
+def check_missing_direction(sender, instance, created, **kwargs):
+    if created and instance.flight_number and not (instance.direction and instance.direction.strip()):
+        send_missing_direction_email(instance)
 
                 
--- utils/post_helper.py
+++ utils/post_helper.py
@@ -19,3 +19,25 @@ def send_post_cancelled_email(instance):
     )
     email.attach_alternative(html_content, "text/html")
     email.send()
+
+
+def send_missing_direction_email(instance):
+    subject = "Booking with Flight Number but Missing Direction"
+    template = "basecamp/html_email-missing-direction.html"
+
+    html_content = render_to_string(template, {
+        'name': instance.name,
+        'email': instance.email,
+        'pickup_date': instance.pickup_date,
+        'flight_number': instance.flight_number,
+    })
+
+    text_content = strip_tags(html_content)
+    email = EmailMultiAlternatives(
+        subject,
+        text_content,
+        '',
+        [RECIPIENT_EMAIL]  
+    )
+    email.attach_alternative(html_content, "text/html")
+    email.send()
