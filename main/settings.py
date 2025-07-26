e52fd5b0a5529a82de681b9b35dd6f07b0f084fb
yhwh371224 <sungkam3@gmail.com>
2025-07-26 11:40:11 +1000

    update

--- main/settings.py
+++ main/settings.py
@@ -189,7 +189,15 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    },
+    'backup': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_USER_PASSWORD'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': '3306',
+    },
 }
 
 CACHES = {
