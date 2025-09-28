13e1a9be72dbf1d4084bce67fda03f00de448235
Sung Kam <sungkam3@gmail.com>
2025-09-28 13:41:37 +1000

    update

--- blog/views.py
+++ blog/views.py
@@ -106,3 +106,21 @@ def xrp_payment_page(request):
     return render(request, "basecamp/includes/xrp_payment.html")
 
 
+# 오류 핸들러
+def custom_bad_request(request, exception):
+    return render(request, "400.html", status=400)
+
+def custom_forbidden(request, exception):
+    return render(request, "403.html", status=403)
+
+def custom_page_not_found(request, exception):
+    return render(request, "404.html", status=404)
+
+def custom_server_error(request):
+    return render(request, "500.html", status=500)
+
+def custom_bad_gateway(request):
+    return render(request, "502.html", status=502)
+
+def custom_under_maintenance(request):
+    return render(request, "503.html", status=503)
