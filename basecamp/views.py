cffe25e6be1cf9d072d24b9f286c28a3c4540fda
yhwh371224 <sungkam3@gmail.com>
2025-10-20 22:54:14 +1100

    update

--- basecamp/views.py
+++ basecamp/views.py
@@ -26,7 +26,7 @@ from basecamp.area_full import get_more_suburbs
 from basecamp.area_home import get_home_suburbs
 
 from utils.pdf import render_to_pdf
-# from utils.date_utils import parse_date_safe
+from utils.date_utils import parse_future_date
 
 
 logger = logging.getLogger(__name__)
@@ -335,10 +335,20 @@ def wrong_date_today(request):
 # Inquiry for airport 
 def inquiry_details(request):
     if request.method == "POST":
+        try:
+            # 필수/선택 날짜 처리
+            pickup_date = parse_future_date(request.POST.get('pickup_date'), "pickup_date")
+            return_pickup_date = parse_future_date(
+                request.POST.get('return_pickup_date'),
+                "return_pickup_date",
+                required=False  # 선택사항
+            )
+        except ValueError as e:
+            return JsonResponse({'success': False, 'error': str(e)})
+        
         name = request.POST.get('name')
         contact = request.POST.get('contact')
-        email = request.POST.get('email')     
-        pickup_date = request.POST.get('pickup_date')   
+        email = request.POST.get('email')             
         flight_number = request.POST.get('flight_number')
         flight_time = request.POST.get('flight_time')
         pickup_time = request.POST.get('pickup_time')
@@ -350,7 +360,6 @@ def inquiry_details(request):
         no_of_passenger = request.POST.get('no_of_passenger')
         no_of_baggage = request.POST.get('no_of_baggage')
         return_direction = request.POST.get('return_direction')   
-        return_pickup_date = request.POST.get('return_pickup_date')
         return_flight_number = request.POST.get('return_flight_number')
         return_flight_time = request.POST.get('return_flight_time')
         return_pickup_time = request.POST.get('return_pickup_time')
