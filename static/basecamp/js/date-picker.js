ef4588023848a241b49c251ef92b245a350e5ad0
yhwh371224 <sungkam3@gmail.com>
2025-10-30 07:14:30 +1100

    update

--- static/basecamp/js/date-picker.js
+++ static/basecamp/js/date-picker.js
@@ -4,23 +4,34 @@
  */
 
 
+// const datePicker = (() => {
+//   let picker = document.querySelectorAll('input.date-picker'); // <-- only inputs      
+//   if (picker.length === 0) return;
+  
+//   for (let i = 0; i < picker.length; i++) {
+
+//     let defaults = {
+//       disableMobile: 'true'
+//     }
+    
+//     let userOptions;
+//     if(picker[i].dataset.datepickerOptions != undefined) userOptions = JSON.parse(picker[i].dataset.datepickerOptions);
+//     let linkedInput = picker[i].classList.contains('date-range') ? {"plugins": [new rangePlugin({ input: picker[i].dataset.linkedInput })]} : '{}';
+//     let options = {...defaults, ...linkedInput, ...userOptions}
+
+//     flatpickr(picker[i], options);
+//   }
+// })();
+
 const datePicker = (() => {
-  let picker = document.querySelectorAll('input.date-picker'); // <-- only inputs
-      
+  let picker = document.querySelectorAll('input.date-picker');      
   if (picker.length === 0) return;
   
-  for (let i = 0; i < picker.length; i++) {
-
-    let defaults = {
-      disableMobile: 'true'
-    }
-    
-    let userOptions;
-    if(picker[i].dataset.datepickerOptions != undefined) userOptions = JSON.parse(picker[i].dataset.datepickerOptions);
-    let linkedInput = picker[i].classList.contains('date-range') ? {"plugins": [new rangePlugin({ input: picker[i].dataset.linkedInput })]} : '{}';
-    let options = {...defaults, ...linkedInput, ...userOptions}
-
-    flatpickr(picker[i], options);
-  }
+  picker.forEach(p => {
+    let defaults = { disableMobile: true };
+    let userOptions = p.dataset.datepickerOptions ? JSON.parse(p.dataset.datepickerOptions) : {};
+    flatpickr(p, {...defaults, ...userOptions});
+  });
 })();
+
 
