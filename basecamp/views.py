c5e79aaee6312e156ac3461c3238a94fc91b2459
yhwh371224 <sungkam3@gmail.com>
2025-10-30 22:05:48 +1100

    update

--- basecamp/views.py
+++ basecamp/views.py
@@ -1577,8 +1577,10 @@ def sending_email_first_detail(request):
                 else:
                     template_name = "basecamp/html_email-confirmation.html"
 
-                html_content = render_to_string("basecamp/html_email-confirmation.html", 
-                                        {'company_name': user.company_name, 'name': user.name, 'contact': user.contact, 'email': user.email, 'email1': user.email1,
+                html_content = render_to_string(
+                                        "basecamp/html_email-confirmation.html", 
+                                        {
+                                            'company_name': user.company_name, 'name': user.name, 'contact': user.contact, 'email': user.email, 'email1': user.email1,
                                             'pickup_date': user.pickup_date, 'flight_number': user.flight_number,
                                             'flight_time': user.flight_time, 'pickup_time': user.pickup_time,
                                             'direction': user.direction, 'street': user.street, 'suburb': user.suburb,
@@ -1586,8 +1588,10 @@ def sending_email_first_detail(request):
                                             'return_direction': user.return_direction, 'return_pickup_date': user.return_pickup_date, 
                                             'return_flight_number': user.return_flight_number, 'return_flight_time': user.return_flight_time, 
                                             'return_pickup_time': user.return_pickup_time,'message': user.message, 'notice': user.notice, 
-                                            'price': user.price, 'paid': user.paid, 'cash': user.cash})
-            
+                                            'price': user.price, 'paid': user.paid, 'cash': user.cash, 'start_point': getattr(user, 'start_point', ''),  
+                                            'return_start_point': getattr(user, 'return_start_point', ''),
+                                        })
+
                 text_content = strip_tags(html_content)
                 email = EmailMultiAlternatives(
                     "Booking confirmation - EasyGo",
@@ -1660,7 +1664,8 @@ def sending_email_second_detail(request):
                 'direction': user.direction, 'street': user.street, 'suburb': user.suburb, 'no_of_passenger': user.no_of_passenger, 'no_of_baggage': user.no_of_baggage,
                 'return_direction': user.return_direction, 'return_pickup_date': user.return_pickup_date, 'return_flight_number': user.return_flight_number, 
                 'return_flight_time': user.return_flight_time, 'return_pickup_time': user.return_pickup_time, 'message': user.message, 'notice': user.notice, 
-                'price': double_price, 'toll': user.toll, 'paid': double_paid, 'cash': user.cash
+                'price': double_price, 'toll': user.toll, 'paid': double_paid, 'cash': user.cash, 'start_point': getattr(user, 'start_point', ''),  
+                'return_start_point': getattr(user, 'return_start_point', ''),
             }
 
             html_content = render_to_string(template_name, context)
