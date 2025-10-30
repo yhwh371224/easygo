2394c708196d5e0584bf80649ca60cda918a15f2
yhwh371224 <sungkam3@gmail.com>
2025-10-30 20:25:45 +1100

    update

--- basecamp/utils.py
+++ basecamp/utils.py
@@ -41,7 +41,7 @@ def parse_date(date_str, field_name="Date", required=True, reference_date=None):
 
 # email_dispatch_detail 
 def handle_email_sending(request, email, subject, template_name, context, email1=None):
-    html_content = render_to_string(template_name, context, request=request)
+    html_content = render_to_string(template_name, context)
     text_content = strip_tags(html_content)
     text_content = text_content.replace('✅', '').replace('🚨', '').replace('💰', '')
     
