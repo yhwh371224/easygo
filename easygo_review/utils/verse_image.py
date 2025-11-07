31a31f5afaa06896ae0dc626087e5c9b8fac8202
yhwh371224 <sungkam3@gmail.com>
2025-11-07 16:18:19 +1100

    update

--- easygo_review/utils/verse_image.py
+++ easygo_review/utils/verse_image.py
@@ -32,7 +32,7 @@ def create_verse_image(verse_text, uploaded_image=None):
     font_path = os.path.join(settings.BASE_DIR, 'static', 'fonts', 'NotoSansKR-Regular.ttf')
 
     # 글씨 크기 자동 조정 (이미지 세로의 50% 이하)
-    max_font_size = H // 24  # 최대 글씨 작게
+    max_font_size = H // 28  # 최대 글씨 작게
     min_font_size = 10
     font_size = max_font_size
     max_width_ratio = 0.8
