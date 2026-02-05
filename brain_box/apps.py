d04b781b7f230f2deb074822045e2280d631f1c3
yhwh371224 <sungkam3@gmail.com>
2026-02-05 20:15:34 +1100

    update

--- brain_box/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-class BrainBoxConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'brain_box'
-    verbose_name = "Brain Box"
--- brain_box/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.calculate_trade, name='calculate_trade'),
-]
