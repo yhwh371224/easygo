1b0ef299e372d816ebbe3a602719ac803d6d065e
yhwh371224 <sungkam3@gmail.com>
2026-02-06 12:48:29 +1100

    update

--- main/urls.py
+++ main/urls.py
@@ -20,7 +20,6 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('paypal/', include('paypal.standard.ipn.urls')),
     path('stripe_webhook/', stripe_webhook, name='stripe_webhook'),
-    path('sitemap.xml', TemplateView.as_view(template_name='sitemap.xml', content_type='application/xml'), name='sitemap'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
