8ee9eddcb67ce4c4f907eb6255d76737745a1dcf
yhwh371224 <sungkam3@gmail.com>
2026-02-18 14:02:25 +1100

    update

--- easygo_review/views.py
+++ easygo_review/views.py
@@ -287,6 +287,9 @@ def index(request):
 
 
 def verify_recaptcha(response, version='v2'):
+    if getattr(settings, 'RECAPTCHA_DISABLED', False):
+        return {'success': True}
+
     if version == 'v2':
         secret_key = settings.RECAPTCHA_V2_SECRET_KEY
     elif version == 'v3':
