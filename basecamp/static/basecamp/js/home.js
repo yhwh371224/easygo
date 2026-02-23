358e4d4fbeee98e916e7b0d1cfd5bc4cbc922564
yhwh371224 <sungkam3@gmail.com>
2026-02-23 17:59:54 +1100

    update

--- basecamp/static/basecamp/js/home.js
+++ basecamp/static/basecamp/js/home.js
@@ -6,10 +6,4 @@ document.addEventListener('DOMContentLoaded', function () {
       location.href = 'https://easygo.s3.ap-southeast-2.amazonaws.com/panel_members.webp';
     });
   }
-
-  // 2. CSP 대응: CSS 비동기 로드 (onload="this.media='all'" 대체)
-  const lazyCss = document.querySelectorAll('.lazy-load-css');
-  lazyCss.forEach(link => {
-    link.media = 'all';
-  });
-});
+})
