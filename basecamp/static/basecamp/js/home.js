c28a3d10b36ce8f05ed5e92d8987358e112db4fd
yhwh371224 <sungkam3@gmail.com>
2026-02-25 07:50:16 +1100

    update

--- basecamp/static/basecamp/js/home.js
+++ basecamp/static/basecamp/js/home.js
@@ -8,22 +8,22 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   // 2. 폼 위치 자동 조정
-  function adjustFormPosition() {
-    const jarallax = document.querySelector('section.jarallax');
-    const form = document.querySelector('.booking-form-top');
-    if (!jarallax || !form) return;
+  // function adjustFormPosition() {
+  //   const jarallax = document.querySelector('section.jarallax');
+  //   const form = document.querySelector('.booking-form-top');
+  //   if (!jarallax || !form) return;
 
-    if (window.innerWidth > 768) {
-      jarallax.style.paddingBottom = '';
-      form.style.marginBottom = '-98px';
-    } else {
-      const formHeight = form.offsetHeight;
-      jarallax.style.paddingBottom = formHeight + 'px';
-      form.style.marginBottom = '-' + (formHeight +25) + 'px';
-    }
-  }
+  //   if (window.innerWidth > 768) {
+  //     jarallax.style.paddingBottom = '';
+  //     form.style.marginBottom = '-98px';
+  //   } else {
+  //     const formHeight = form.offsetHeight;
+  //     jarallax.style.paddingBottom = formHeight + 'px';
+  //     form.style.marginBottom = '-' + (formHeight +25) + 'px';
+  //   }
+  // }
 
-  window.addEventListener('load', adjustFormPosition);
-  window.addEventListener('resize', adjustFormPosition);
-  adjustFormPosition(); // 페이지 로드시 즉시 실행
+  // window.addEventListener('load', adjustFormPosition);
+  // window.addEventListener('resize', adjustFormPosition);
+  // adjustFormPosition(); // 페이지 로드시 즉시 실행
 });
