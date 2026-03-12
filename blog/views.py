09bf211e9b61abd70e07801e18a16a128c0b5a8b
yhwh371224 <sungkam3@gmail.com>
2026-03-12 18:53:58 +1100

    update

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 from django.utils import timezone
 
 from main.settings import RECIPIENT_EMAIL
-from .models import XrpPayment, Post
+# from .models import XrpPayment, Post
 # from .tasks import send_xrp_internal_email, send_xrp_customer_email
 
 
@@ -18,6 +18,26 @@ def email_to_tag(email: str) -> int:
     import hashlib
     hash_int = int(hashlib.md5(email.encode()).hexdigest(), 16)
     return hash_int % (2**32)
+
+
+# 공통 오류 핸들러
+def custom_bad_request(request, exception):
+    return render(request, "400.html", status=400)
+
+def custom_forbidden(request, exception):
+    return render(request, "403.html", status=403)
+
+def custom_page_not_found(request, exception):
+    return render(request, "404.html", status=404)
+
+def custom_server_error(request):
+    return render(request, "500.html", status=500)
+
+def custom_bad_gateway(request):
+    return render(request, "502.html", status=502)
+
+def custom_under_maintenance(request):
+    return render(request, "503.html", status=503)
 
 
 # def xrp_payment(request):
@@ -112,21 +132,4 @@ def email_to_tag(email: str) -> int:
 #     return render(request, "basecamp/includes/xrp_payment.html")
 
 
-# 공통 오류 핸들러
-def custom_bad_request(request, exception):
-    return render(request, "400.html", status=400)
 
-def custom_forbidden(request, exception):
-    return render(request, "403.html", status=403)
-
-def custom_page_not_found(request, exception):
-    return render(request, "404.html", status=404)
-
-def custom_server_error(request):
-    return render(request, "500.html", status=500)
-
-def custom_bad_gateway(request):
-    return render(request, "502.html", status=502)
-
-def custom_under_maintenance(request):
-    return render(request, "503.html", status=503)
