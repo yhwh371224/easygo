a9362c028b1eb2fda480e4a9aa16e5a70296d046
yhwh371224 <sungkam3@gmail.com>
2026-03-16 18:09:20 +1100

    update

---
+++ email_agent/prompts/user_template.py
@@ -0,0 +1,24 @@
+from django.core.cache import cache
+from basecamp.area_full import get_more_suburbs
+
+
+def build_user_prompt(sender, subject, body, history_text, suburb_list):
+    return f"""[Available Suburbs]
+{suburb_list}
+
+{history_text}
+
+[New Email]
+From: {sender}
+Subject: {subject}
+Body:
+{body}"""
+
+
+def get_suburb_list():
+    cached = cache.get("suburb_list")
+    if cached:
+        return cached
+    suburbs = list(get_more_suburbs().keys())
+    cache.set("suburb_list", suburbs, timeout=3600)  # 1시간
+    return suburbs
