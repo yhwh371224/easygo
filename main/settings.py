5f64827ab2f64bcdd405f2a08a5b10eaf7b67f97
yhwh371224 <sungkam3@gmail.com>
2026-03-30 20:24:42 +1100

    update

--- main/settings.py
+++ main/settings.py
@@ -120,7 +120,6 @@ INSTALLED_APPS = [
     'storages',
     'corsheaders',
     'paypal.standard.ipn',
-    'meta',
     'markdownx',
     'csp',
     'axes',    
@@ -370,9 +369,6 @@ CORS_ALLOWED_ORIGINS = [
 CORS_ALLOW_METHODS = [ 'GET', 'POST', ]
 
 CORS_ALLOW_HEADERS = [ 'Content-Type', 'X-CSRFToken', ]
-
-META_SITE_PROTOCOL = 'https'
-META_USE_OG_PROPERTIES = True 
 
 APPEND_SLASH = True
 
