8d57cb76624b5a0cc634fcafc4acda80518324a9
yhwh371224 <sungkam3@gmail.com>
2026-04-04 17:20:27 +1100

    update

--- posting_agent/admin.py
+++ posting_agent/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from posting_agent.models import BlogPost
 
-# Register your models here.
+
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'blog_status', 'published_at', 'created_at']
+    list_filter = ['blog_status']
+    search_fields = ['title']
+    prepopulated_fields = {'slug': ('title',)}
