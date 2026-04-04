f8622d7ffb00fa049795ad7796f422595b8229e8
yhwh371224 <sungkam3@gmail.com>
2026-04-04 18:12:38 +1100

    update

--- posting_agent/blog_poster.py
+++ posting_agent/blog_poster.py
@@ -2,10 +2,10 @@ from django.core.files.base import ContentFile
 from django.utils import timezone
 from posting_agent.models import BlogPost
 
-
 def save_blog_post(content: dict, image_bytes: bytes, filename: str) -> BlogPost:
     post = BlogPost(
         title=content['title'],
+        slug=content['topic_slug'],  # 추가!
         seo_content=content['seo_content'],
         social_content=content['social_content'],
         gmb_content=content['gmb_content'],
@@ -14,8 +14,6 @@ def save_blog_post(content: dict, image_bytes: bytes, filename: str) -> BlogPost
         published_at=timezone.now(),
     )
     post.save()
-
     if image_bytes:
         post.image.save(filename, ContentFile(image_bytes), save=True)
-
     return post
