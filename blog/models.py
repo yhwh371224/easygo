eec9bc4b34e962af60964a735ac22458e4965f25
yhwh371224 <sungkam3@gmail.com>
2026-04-23 13:49:22 +1000

    update

--- blog/models.py
+++ blog/models.py
@@ -23,6 +23,7 @@ class VirtualNumber(models.Model):
 
 class Driver(models.Model):
     user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True, blank=True)
+    must_change_password = models.BooleanField(default=True)
     driver_name = models.CharField(max_length=100, blank=True, null=True)
     driver_contact = models.CharField(max_length=50, blank=True, null=True)
     driver_email = models.EmailField(blank=True, null=True)
--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views, driver_views
+from . import driver_views
 
 
 app_name = 'blog'
@@ -11,5 +11,6 @@ urlpatterns = [
     path('driver/logout/', driver_views.driver_logout, name='driver_logout'),
     path('driver/', driver_views.driver_dashboard, name='driver_dashboard'),
     path('driver/complete/<int:post_id>/', driver_views.driver_complete_trip, name='driver_complete_trip'),
+    path('driver/change-password/', driver_views.driver_change_password, name='driver_change_password'),
 ]
 
