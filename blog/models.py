102b66e99eaee995bf2f51513f16e4ee2c7d19d7
yhwh371224 <sungkam3@gmail.com>
2026-04-23 16:33:52 +1000

    update

--- blog/models.py
+++ blog/models.py
@@ -196,6 +196,9 @@ class XrpPayment(models.Model):
 class PhoneMapping(models.Model):
     from_number = models.CharField(max_length=20, db_index=True)
     to_number = models.CharField(max_length=20)
+    driver_name = models.CharField(max_length=100, blank=True, null=True)
+    pickup_date = models.DateField(blank=True, null=True)
+    pickup_time = models.CharField(max_length=30, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
