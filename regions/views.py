4993e83f4e8a82f353a80d674a5179cae41c6929
yhwh371224 <sungkam3@gmail.com>
2026-04-25 18:14:29 +1000

    feat: 멜번 템플릿 및 뷰 추가 & return_trip에 region 상속 추가

--- regions/views.py
+++ regions/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Region
+from .models import Region, RegionSuburb
 
 
 def region_home(request, region_slug):
     region = get_object_or_404(Region, slug=region_slug, is_active=True)
-    return render(request, 'regions/home.html', {'region': region})
+    suburbs = region.suburbs.filter(is_active=True).order_by('zone', 'name')
+    return render(request, 'regions/home.html', {
+        'region': region,
+        'suburbs': suburbs,
+    })
 
 
 def region_booking(request, region_slug):
@@ -17,9 +21,10 @@ def region_confirmation(request, region_slug):
     return render(request, 'regions/confirmation.html', {'region': region})
 
 
-def airport_shuttle_landing(request, region_slug, suburb):
+def airport_shuttle_suburb(request, region_slug, suburb_slug):
     region = get_object_or_404(Region, slug=region_slug, is_active=True)
-    return render(request, 'regions/airport_shuttle.html', {
+    suburb = get_object_or_404(RegionSuburb, region=region, slug=suburb_slug, is_active=True)
+    return render(request, 'regions/airport_shuttle_suburb.html', {
         'region': region,
         'suburb': suburb,
     })
