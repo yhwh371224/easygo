ec8af164ebc950b8a6b0e8b383607df919d517b6
yhwh371224 <sungkam3@gmail.com>
2026-05-04 22:47:19 +1000

    update

--- email_agent/views.py
+++ email_agent/views.py
@@ -1,4 +1,3 @@
-# email_agent/views.py
 import json
 import base64
 from django.http import HttpResponse
@@ -7,15 +6,19 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from .tasks import gmail_watch_topic
 
+
 @method_decorator(csrf_exempt, name='dispatch')
 class GmailWebhookView(View):
     def post(self, request):
-        data = json.loads(request.body)
-        pubsub_message = data.get('message', {})
-        if pubsub_message:
-            message_data = base64.b64decode(
-                pubsub_message['data']
-            ).decode('utf-8')
-            payload = json.loads(message_data)
-            gmail_watch_topic.apply_async(args=[payload], countdown=10)
+        try:
+            data = json.loads(request.body)
+            pubsub_message = data.get('message', {})
+            if pubsub_message:
+                message_data = base64.b64decode(
+                    pubsub_message['data']
+                ).decode('utf-8')
+                payload = json.loads(message_data)
+                gmail_watch_topic.apply_async(args=[payload], countdown=10)
+        except Exception:
+            pass
         return HttpResponse(status=200)
