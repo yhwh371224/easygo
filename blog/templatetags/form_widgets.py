62bf18514becdf97b66ff2ecd777f2775eed38ba
yhwh371224 <sungkam3@gmail.com>
2026-05-08 08:20:03 +1000

    replace time inputs with 12h AM/PM picker widget

---
+++ blog/templatetags/form_widgets.py
@@ -0,0 +1,19 @@
+from django import template
+register = template.Library()
+
+
+@register.inclusion_tag('widgets/time_picker.html')
+def time_picker(field_name, label, value=''):
+    hour, minute, ampm = '', '', 'AM'
+    if value:
+        try:
+            h, m = int(value[:2]), int(value[3:5])
+            minute = m
+            if h == 0:       hour, ampm = 12, 'AM'
+            elif h < 12:     hour, ampm = h,  'AM'
+            elif h == 12:    hour, ampm = 12, 'PM'
+            else:            hour, ampm = h - 12, 'PM'
+        except (ValueError, IndexError):
+            pass
+    return {'field_name': field_name, 'label': label,
+            'hour': hour, 'minute': str(minute).zfill(2), 'ampm': ampm}
