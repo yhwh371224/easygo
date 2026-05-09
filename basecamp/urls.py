b1aa416f1350f4c5cca0175f72c1b677596c3f27
yhwh371224 <sungkam3@gmail.com>
2026-05-10 00:33:02 +1000

    update

--- basecamp/urls.py
+++ basecamp/urls.py
@@ -57,7 +57,6 @@ urlpatterns = [
     path('more_suburbs_maxi_taxi/', views.more_suburbs_maxi_taxi, name='more_suburbs_maxi_taxi'),
     path('more_suburbs/', views.more_suburbs, name='more_suburbs'),
     path('more_suburbs1/', views.more_suburbs1, name='more_suburbs1'),
-    path('p2p_detail/', views.p2p_detail, name='p2p_detail'),
     path('p2p_single/', views.p2p_single, name="p2p_single"),    
     path('payment_cancel/', views.payment_cancel, name='payment_cancel'),   
     path('payment_options/', views.payment_options, name='payment_options'),
