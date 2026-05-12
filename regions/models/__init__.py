b73f311591767231b4f33b0b4331ee156b2578d8
yhwh371224 <sungkam3@gmail.com>
2026-05-12 16:02:06 +1000

    update

--- regions/models/__init__.py
+++ regions/models/__init__.py
@@ -1,5 +1,5 @@
 from .region import Country, Region
-from .airport import Airport, Terminal, TerminalPickupPoint, PickupPointMap
+from .airport import Airport, Terminal, TerminalPickupPoint, PickupPointMap, CruiseTerminal
 from .suburb import RegionSuburb
 from .logs import RequestLog
 
@@ -11,5 +11,6 @@ __all__ = [
     "TerminalPickupPoint",
     "PickupPointMap",
     "RegionSuburb",
+    "CruiseTerminal",
     "RequestLog",
 ]
