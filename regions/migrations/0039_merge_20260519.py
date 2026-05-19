61b06f7b1ae0ec565d25d2fd51a50a3a508c9cdb
yhwh371224 <sungkam3@gmail.com>
2026-05-19 18:54:34 +1000

    update

---
+++ regions/migrations/0039_merge_20260519.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('regions', '0038_merge_20260519_1456'),
+        ('regions', '0038_round_suburb_prices_to_nearest_5'),
+    ]
+
+    operations = [
+    ]
