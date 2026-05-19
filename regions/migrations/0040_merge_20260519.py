c891aa9ec6e6dc2cce18b21747bbc3da07559c91
yhwh371224 <sungkam3@gmail.com>
2026-05-19 19:06:33 +1000

    update

---
+++ regions/migrations/0040_merge_20260519.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('regions', '0036_merge_20260519_0940'),
+        ('regions', '0039_merge_20260519_1848'),
+    ]
+
+    operations = [
+    ]
